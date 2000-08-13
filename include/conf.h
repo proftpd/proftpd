df6d76d21a39f28ec982b3a1db34e73350abcf06
macgyver <macgyver>
2000-08-13 15:19:39 +0000

    Some configure and compilation cleanups.

--- include/conf.h
+++ include/conf.h
@@ -18,8 +18,27 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  */
 
+/*
+ * ProFTPD - FTP server daemon
+ * Copyright (C) 1999, 2000 MacGyver aka Habeeb J. Dihu <macgyver@tos.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 /* Generic configuration and standard header file includes.
- * $Id: conf.h,v 1.6 2000-08-02 05:25:24 macgyver Exp $
+ * $Id: conf.h,v 1.7 2000-08-13 15:19:39 macgyver Exp $
  */
 
 #ifndef __CONF_H
