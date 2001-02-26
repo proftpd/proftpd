6cca7cb159b7d04d841b017c01c6d6af7305d254
flood <flood>
2001-02-26 20:13:43 +0000

    Final commit to 1.2.0-final release

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.0rc3"
+#define VERSION                 "1.2.0"
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.0rc3 (CVS)"
-#define INTERNAL_VERSION        0x01020000
+#define VERSION_STATUS          "1.2.0-final (CVS)"
+#define INTERNAL_VERSION        0x01020001
