73cb0a502519b2111da46b15c0fea4c199ab89af
flood <flood>
2001-03-24 15:59:31 +0000

    Bumped version number to 1.2.2rc1.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.1"
+#define VERSION                 "1.2.2rc1"
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.1 (CVS)"
+#define VERSION_STATUS          "1.2.2rc1 (CVS)"
 #define INTERNAL_VERSION        0x01020001
