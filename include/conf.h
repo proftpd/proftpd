b95d49864b7b309409cf5fe717901bf3a32bd09d
flood <flood>
2001-08-16 20:00:51 +0000

    Double license in header conf.h

--- include/conf.h
+++ include/conf.h
@@ -23,27 +23,8 @@
  * the source code for OpenSSL in the source distribution.
  */
 
-/*
- * ProFTPD - FTP server daemon
- * Copyright (C) 1999, 2000 MacGyver aka Habeeb J. Dihu <macgyver@tos.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
- */
-
 /* Generic configuration and standard header file includes.
- * $Id: conf.h,v 1.10 2001-06-18 17:12:45 flood Exp $
+ * $Id: conf.h,v 1.11 2001-08-16 20:00:51 flood Exp $
  */
 
 #ifndef __CONF_H
