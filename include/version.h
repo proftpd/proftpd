aa6af232df4dcfd5da8b4742287be51f5072c222
flood <flood>
2001-10-19 15:28:23 +0000

    1.2.4

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.3"
+#define VERSION                 "1.2.4"
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.3 (CVS)"
+#define VERSION_STATUS          "1.2.4 (CVS)"
 #define INTERNAL_VERSION        0x01030000
