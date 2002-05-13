266a7bd59bd7a4853f9c7c0ce92413ebe3f9ffef
castaglia <castaglia>
2002-05-13 14:05:35 +0000

    TJ: updated NEWS, version.h with date of 1.2.5rc2 release

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.5rc1"
+#define VERSION                 "1.2.5rc2"
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.5rc1 (CVS)"
+#define VERSION_STATUS          "1.2.5rc2 (release)"
 #define INTERNAL_VERSION        0x01030000
