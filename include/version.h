7fa2e1a74b402fbdb00c8a66f9b8dd8cafbc9a5f
castaglia <castaglia>
2002-05-30 20:13:14 +0000

    Damn, I forgot to update this file.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.5rc2"
+#define VERSION                 "1.2.5rc3"
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.5rc2 (release)"
+#define VERSION_STATUS          "1.2.5rc3 (release)"
 #define INTERNAL_VERSION        0x01030000
