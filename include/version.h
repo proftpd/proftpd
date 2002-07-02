714d066058bc246c91884693d26b30c0d4eefb3f
castaglia <castaglia>
2002-07-02 18:25:36 +0000

    Preparing for release of 1.2.6rc1

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #include "buildstamp.h"
 
-#define VERSION                 "1.2.5"
+#define VERSION                 "1.2.6rc1"
 
 /* VERSION_STATUS is reported by --version-status
  * don't ask why
  */
-#define VERSION_STATUS          "1.2.5 (CVS)"
+#define VERSION_STATUS          "1.2.6rc1"
 
 /* INTERNAL_VERSION is used for checking for scoreboard compatibility
  */
