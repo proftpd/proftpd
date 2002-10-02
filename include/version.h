cb158938e30770e147258aedb608d9320feb1a91
castaglia <castaglia>
2002-10-02 20:35:01 +0000

    Updating the version, preparing for the next CVS/release.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define PROFTPD_VERSION_NUMBER		0x0001020701
-#define PROFTPD_VERSION_TEXT		"1.2.7rc1"
+#define PROFTPD_VERSION_NUMBER		0x0001020702
+#define PROFTPD_VERSION_TEXT		"1.2.7rc2"
 
 /* PR_STATUS is reported by --version-status -- don't ask why
  */
-#define PR_STATUS          		"(devel)"
+#define PR_STATUS          		"(CVS)"
