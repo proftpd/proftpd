a6855c0b897677505fe84b3768c908e4d878a21b
castaglia <castaglia>
2002-12-05 15:30:22 +0000

    Preparing for release of 1.2.7.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define PROFTPD_VERSION_NUMBER		0x0001020703
-#define PROFTPD_VERSION_TEXT		"1.2.7rc3"
+#define PROFTPD_VERSION_NUMBER		0x0001020704
+#define PROFTPD_VERSION_TEXT		"1.2.7"
 
 /* PR_STATUS is reported by --version-status -- don't ask why
  */
-#define PR_STATUS          		"(devel)"
+#define PR_STATUS          		"(stable)"
