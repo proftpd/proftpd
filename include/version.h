42c6c74973a95fc1473878fc6f7c64e7a759aba2
castaglia <castaglia>
2002-12-05 19:03:23 +0000

    Now that 1.2.7 has been released, update the version info in CVS.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
-#define PROFTPD_VERSION_NUMBER		0x0001020704
-#define PROFTPD_VERSION_TEXT		"1.2.7"
+#define PROFTPD_VERSION_NUMBER		0x0001020801
+#define PROFTPD_VERSION_TEXT		"1.2.8rc1"
 
 /* PR_STATUS is reported by --version-status -- don't ask why
  */
-#define PR_STATUS          		"(stable)"
+#define PR_STATUS          		"(CVS)"
