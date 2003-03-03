0b93c7661afce9b7ca516ea16230e036eb8261c6
castaglia <castaglia>
2003-03-03 00:31:36 +0000

    Adding FreeBSD-5.0 to the #defines for the socket/privs fun.

--- src/inet.c
+++ src/inet.c
@@ -447,8 +447,8 @@ static conn_t *inet_initialize_connection(pool *p, xaset_t *servers, int fd,
      */
 
 #if defined(SOLARIS2) || defined(FREEBSD2) || defined(FREEBSD3) || \
-    defined(FREEBSD4) || defined(__OpenBSD__) || defined(__NetBSD__) || \
-    defined(SYSV4_2MP) || defined(SCO3)
+    defined(FREEBSD4) || defined(FREEBSD5) || defined(__OpenBSD__) || \
+    defined(__NetBSD__) || defined(SYSV4_2MP) || defined(SCO3)
 # ifdef SOLARIS2
     if (port != INPORT_ANY && port < 1024) {
 # endif
@@ -462,8 +462,8 @@ static conn_t *inet_initialize_connection(pool *p, xaset_t *servers, int fd,
     fd = socket(AF_INET, SOCK_STREAM, tcp_proto);
 
 #if defined(SOLARIS2) || defined(FREEBSD2) || defined(FREEBSD3) || \
-    defined(FREEBSD4) || defined(__OpenBSD__) || defined(__NetBSD__) || \
-    defined(SYSV4_2MP) || defined(SCO3)
+    defined(FREEBSD4) || defined(FREEBSD5) || defined(__OpenBSD__) ||
+    defined(__NetBSD__) || defined(SYSV4_2MP) || defined(SCO3)
 # ifdef SOLARIS2
     if (port != INPORT_ANY && port < 1024) {
 # endif
