3e3a8f251594466e13ea0598d172fe0ac1346b59
castaglia <castaglia>
2003-12-11 20:30:53 +0000

    Bump the mod_quotatab version, for clarity.

--- contrib/mod_quotatab.h
+++ contrib/mod_quotatab.h
@@ -29,7 +29,7 @@
  * ftp://pooh.urbanrage.com/pub/c/.  This module, however, has been written
  * from scratch to implement quotas in a different way.
  *
- * $Id: mod_quotatab.h,v 1.1 2003-12-03 07:39:36 castaglia Exp $
+ * $Id: mod_quotatab.h,v 1.2 2003-12-11 20:30:53 castaglia Exp $
  */
 
 #ifndef MOD_QUOTATAB_H
@@ -38,7 +38,7 @@
 #include "conf.h"
 #include "privs.h"
 
-#define MOD_QUOTATAB_VERSION "mod_quotatab/1.2.11"
+#define MOD_QUOTATAB_VERSION "mod_quotatab/1.2.12"
 
 /* Make sure the version of proftpd is as necessary. */
 #if PROFTPD_VERSION_NUMBER < 0x0001020902
