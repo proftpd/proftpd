548d6ae59bc55b9801dc2022e7cd1ee40c44d830
castaglia <castaglia>
2004-02-17 03:53:09 +0000

    Bug#2105 - Remove Authoritative directives.

--- modules/mod_auth_pam.c
+++ modules/mod_auth_pam.c
@@ -35,7 +35,7 @@
  *
  * -- DO NOT MODIFY THE TWO LINES BELOW --
  * $Libraries: -lpam$
- * $Id: mod_auth_pam.c,v 1.4 2003-11-09 21:09:59 castaglia Exp $
+ * $Id: mod_auth_pam.c,v 1.5 2004-02-17 03:53:09 castaglia Exp $
  */
 
 #include "conf.h"
@@ -437,19 +437,7 @@ MODRET set_authpam(cmd_rec *cmd) {
 }
 
 MODRET set_authpamauthoritative(cmd_rec *cmd) {
-  int bool = -1;
-  config_rec *c = NULL;
-
-  CHECK_ARGS(cmd, 1);
-  CHECK_CONF(cmd, CONF_ROOT|CONF_VIRTUAL|CONF_GLOBAL);
-
-  if ((bool = get_boolean(cmd, 1)) == -1)
-    CONF_ERROR(cmd, "expected Boolean parameter");
-
-  c = add_config_param(cmd->argv[0], 1, NULL);
-  c->argv[0] = pcalloc(c->pool, sizeof(unsigned char));
-  *((unsigned char *) c->argv[0]) = bool;
-
+  pr_log_auth(PR_LOG_WARNING, "warning: AuthPAMAuthoritative is deprecated");
   return HANDLED(cmd);
 }
 
@@ -469,8 +457,11 @@ static authtable auth_pam_authtab[] = {
 
 static conftable auth_pam_conftab[] = {
   { "AuthPAM",			set_authpam,			NULL },
-  { "AuthPAMAuthoritative",	set_authpamauthoritative,	NULL },
   { "AuthPAMConfig",		set_authpamconfig,		NULL },
+
+  /* Deprecated */
+  { "AuthPAMAuthoritative",	set_authpamauthoritative,	NULL },
+
   { NULL, NULL, NULL}
 };
 
