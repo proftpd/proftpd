102555580f8f4c83fc8104e55cc789a7648e6c1b
castaglia <castaglia>
2004-04-28 21:00:14 +0000

    Set version back to CVS status for rc2.

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001021001
-#define PROFTPD_VERSION_TEXT		"1.2.10rc1"
+#define PROFTPD_VERSION_NUMBER		0x0001021002
+#define PROFTPD_VERSION_TEXT		"1.2.10rc2"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(devel)"
+#define PR_STATUS          		"(CVS)"
