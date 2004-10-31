f49b002dd25a847d21461c97ba49f6796a3f98f2
castaglia <castaglia>
2004-10-31 19:37:50 +0000

    As Daniel noted, it should be "1.3.0", not just "1.3".

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 
 /* Application version (in various forms) */
 #define PROFTPD_VERSION_NUMBER		0x0001030001
-#define PROFTPD_VERSION_TEXT		"1.3rc1"
+#define PROFTPD_VERSION_TEXT		"1.3.0rc1"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
