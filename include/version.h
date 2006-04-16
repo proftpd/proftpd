e4d6aebd473b9b76e175d58fc88383edca7321c0
castaglia <castaglia>
2006-04-16 22:12:42 +0000

    Now that 1.3.0 is out, we can start on 1.3.1.

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030006
-#define PROFTPD_VERSION_TEXT		"1.3.0"
+#define PROFTPD_VERSION_NUMBER		0x0001030101
+#define PROFTPD_VERSION_TEXT		"1.3.1rc1"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(stable)"
+#define PR_STATUS          		"(CVS)"
