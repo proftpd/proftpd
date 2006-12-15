b2493a0e8c194874746ef16b3913f0ed62e22caa
castaglia <castaglia>
2006-12-15 18:46:29 +0000

    Make sure to reference the gettext() function only when NLS support has been requested.  Otherwise, confusing linker errors result.

--- lib/getopt.c
+++ lib/getopt.c
@@ -76,7 +76,7 @@
 # endif
 #endif
 
-#ifndef _
+#if defined(PR_USE_NLS) && !defined(_)
 /* This is for other GNU distributions with internationalized messages.
    When compiling libc, the _ macro is predefined.  */
 # ifdef HAVE_LIBINTL_H
@@ -85,6 +85,8 @@
 # else
 #  define _(msgid)	(msgid)
 # endif
+#else
+# define _(msgid)	(msgid)
 #endif
 
 /* This version of `getopt' appears to the caller like standard Unix `getopt'
