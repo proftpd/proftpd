82937d566749794b2f302824db46c4e0a7bb3098
castaglia <castaglia>
2006-12-18 23:09:42 +0000

    Adding tracing of NetACLs.

--- src/netacl.c
+++ src/netacl.c
@@ -23,7 +23,7 @@
  */
 
 /* Network ACL routines
- * $Id: netacl.c,v 1.13 2006-09-29 16:38:16 castaglia Exp $
+ * $Id: netacl.c,v 1.14 2006-12-18 23:09:42 castaglia Exp $
  */
 
 #include "conf.h"
@@ -32,12 +32,15 @@ extern int ServerUseReverseDNS;
 
 struct pr_netacl_t {
   pr_netacl_type_t type;
+  const char *aclstr;
 
   char *pattern;
   int negated;
   pr_netaddr_t *addr;
   unsigned int masklen;
 };
+
+static const char *trace_channel = "netacl";
 
 pr_netacl_type_t pr_netacl_get_type(pr_netacl_t *acl) {
   return acl->type;
@@ -50,30 +53,38 @@ int pr_netacl_match(pr_netacl_t *acl, pr_netaddr_t *addr) {
 
   switch (acl->type) {
     case PR_NETACL_TYPE_ALL:
-      pr_log_debug(DEBUG10, "NetACL: matched 'ALL'");
+      pr_trace_msg(trace_channel, 10, "addr '%s' matched rule 'ALL'",
+        pr_netaddr_get_ipstr(addr));
       return 1;
 
     case PR_NETACL_TYPE_NONE:
-      pr_log_debug(DEBUG10, "NetACL: matched 'NONE'");
+      pr_trace_msg(trace_channel, 10, "addr '%s' matched rule 'NONE'",
+        pr_netaddr_get_ipstr(addr));
       return -1;
 
     case PR_NETACL_TYPE_IPMASK:
       if (pr_netaddr_ncmp(addr, acl->addr, acl->masklen) == 0) {
-        pr_log_debug(DEBUG10, "NetACL: matched IP/mask");
+        pr_trace_msg(trace_channel, 10, "addr '%s' matched IP mask rule '%s'",
+          pr_netaddr_get_ipstr(addr), acl->aclstr);
         return 1;
       }
       break;
 
     case PR_NETACL_TYPE_IPMATCH:
       if (pr_netaddr_cmp(addr, acl->addr) == 0) {
-        pr_log_debug(DEBUG10, "NetACL: matched IP address");
+        pr_trace_msg(trace_channel, 10,
+          "addr '%s' matched IP address rule '%s'",
+          pr_netaddr_get_ipstr(addr), acl->aclstr);
         return 1;
       }
       break;
  
     case PR_NETACL_TYPE_DNSMATCH:
       if (strcmp(pr_netaddr_get_dnsstr(addr), acl->pattern) == 0) {
-        pr_log_debug(DEBUG10, "NetACL: matched DNS name");
+        pr_trace_msg(trace_channel, 10,
+          "addr '%s' (%s) matched DNS name rule '%s'",
+          pr_netaddr_get_ipstr(addr), pr_netaddr_get_dnsstr(addr),
+          acl->aclstr);
         return 1;
       }
       break;
@@ -81,17 +92,29 @@ int pr_netacl_match(pr_netacl_t *acl, pr_netaddr_t *addr) {
     case PR_NETACL_TYPE_IPGLOB:
       if (pr_netaddr_fnmatch(addr, acl->pattern,
           PR_NETADDR_MATCH_IP) == TRUE) {
-        pr_log_debug(DEBUG10, "NetACL: matched IP glob pattern");
+        pr_trace_msg(trace_channel, 10,
+          "addr '%s' matched IP glob rule '%s'",
+          pr_netaddr_get_ipstr(addr), acl->aclstr);
         return 1;
       }
       break;
 
     case PR_NETACL_TYPE_DNSGLOB:
-      if (ServerUseReverseDNS &&
-          pr_netaddr_fnmatch(addr, acl->pattern,
+      if (ServerUseReverseDNS) {
+        if (pr_netaddr_fnmatch(addr, acl->pattern,
             PR_NETADDR_MATCH_DNS) == TRUE) {
-        pr_log_debug(DEBUG10, "NetACL: matched DNS glob pattern");
-        return 1;
+          pr_trace_msg(trace_channel, 10,
+            "addr '%s' (%s) matched DNS glob rule '%s'",
+            pr_netaddr_get_ipstr(addr), pr_netaddr_get_dnsstr(addr),
+            acl->aclstr);
+          return 1;
+        }
+
+      } else {
+        pr_trace_msg(trace_channel, 10,
+          "skipping comparing addr '%s' (%s) against DNS glob rule '%s' "
+          "because UseReverseDNS is off", pr_netaddr_get_ipstr(addr),
+          pr_netaddr_get_dnsstr(addr), acl->aclstr);
       }
       break;
 
@@ -104,7 +127,7 @@ int pr_netacl_match(pr_netacl_t *acl, pr_netaddr_t *addr) {
 
 pr_netacl_t *pr_netacl_create(pool *p, char *aclstr) {
   pr_netacl_t *acl;
-  char *cp;
+  char *cp, *aclstr_dup;
 
   if (!p || !aclstr) {
     errno = EINVAL;
@@ -113,6 +136,8 @@ pr_netacl_t *pr_netacl_create(pool *p, char *aclstr) {
 
   /* Parse the given rule into a netacl object. */
   acl = pcalloc(p, sizeof(pr_netacl_t));
+
+  aclstr_dup = pstrdup(p, aclstr);
 
   if (strcasecmp(aclstr, "all") == 0) {
     acl->type = PR_NETACL_TYPE_ALL;
@@ -184,6 +209,8 @@ pr_netacl_t *pr_netacl_create(pool *p, char *aclstr) {
              * If the mask is 32 bits or more, warn the admin.
              */
             pr_log_pri(PR_LOG_WARNING, "warning: possibly using IPv6-style netmask on IPv4-mapped IPv6 address, which will not work as expected");
+            pr_trace_msg(trace_channel, 1, "possibly using IPv6-style netmask on IPv4-mapped IPv6 address (%s), which will not work as expected", aclstr);
+
             break;
           }
         }
@@ -195,7 +222,8 @@ pr_netacl_t *pr_netacl_create(pool *p, char *aclstr) {
     }
 
 #ifdef PR_USE_IPV6
-  } else if (pr_netaddr_use_ipv6() && strspn(aclstr, "0123456789ABCDEFabcdef.:") != strlen(aclstr)) {
+  } else if (pr_netaddr_use_ipv6() &&
+             strspn(aclstr, "0123456789ABCDEFabcdef.:") != strlen(aclstr)) {
 #else
   } else if (strspn(aclstr, "0123456789.") != strlen(aclstr)) {
 #endif /* PR_USE_IPV6 */
@@ -244,6 +272,7 @@ pr_netacl_t *pr_netacl_create(pool *p, char *aclstr) {
     }
   }
 
+  acl->aclstr = aclstr_dup;
   return acl;
 }
 
@@ -273,6 +302,9 @@ pr_netacl_t *pr_netacl_dup(pool *p, pr_netacl_t *acl) {
   }
 
   acl2->masklen = acl->masklen;
+
+  if (acl->aclstr)
+    acl2->aclstr = pstrdup(p, acl->aclstr);
 
   return acl2;
 }
