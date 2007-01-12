b9519f94d57f0195cd7b34fe5a50659ab5497d48
castaglia <castaglia>
2007-01-12 05:40:37 +0000

    Correct the license wording to mention The ProFTPD Project as primary copyright holder.

--- src/proctitle.c
+++ src/proctitle.c
@@ -16,15 +16,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  *
- * As a special exemption, Public Flood Software/MacGyver aka Habeeb J. Dihu
- * and other respective copyright holders give permission to link this program
- * with OpenSSL, and distribute the resulting executable, without including
- * the source code for OpenSSL in the source distribution.
+ * As a special exemption, The ProFTPD Project team and other respective
+ * copyright holders give permission to link this program with OpenSSL, and
+ * distribute the resulting executable, without including the source code for
+ * OpenSSL in the source distribution.
  */
 
 /*
  * Proctitle management
- * $Id: proctitle.c,v 1.1 2007-01-11 19:16:35 castaglia Exp $
+ * $Id: proctitle.c,v 1.2 2007-01-12 05:40:37 castaglia Exp $
  */
 
 #include "conf.h"
