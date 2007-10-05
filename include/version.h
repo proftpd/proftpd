35033e353f01f3b3474dd37d118261f9cd599b08
castaglia <castaglia>
2007-10-05 16:40:48 +0000

    Start of a new release cycle (1.3.2).

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030105
-#define PROFTPD_VERSION_TEXT		"1.3.1"
+#define PROFTPD_VERSION_NUMBER		0x0001030201
+#define PROFTPD_VERSION_TEXT		"1.3.2rc1"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(stable)"
+#define PR_STATUS          		"(CVS)"
