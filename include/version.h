abbacf980e35f3f974f78b61062b277742545a15
castaglia <castaglia>
2008-02-19 02:29:33 +0000

    Make it possible to get versions (number, string) at runtime, for runtime comparisons (e.g. by shared modules at load time).  These functions complement the existing buildtime version macros.

--- include/version.h
+++ include/version.h
@@ -7,5 +7,9 @@
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
 
+unsigned long pr_version_get_module_api_number(void);
+unsigned long pr_version_get_number(void);
+const char *pr_version_get_str(void);
+
 /* PR_STATUS is reported by --version-status -- don't ask why */
 #define PR_STATUS          		"(CVS)"
