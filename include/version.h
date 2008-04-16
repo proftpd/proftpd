17085c248eb782d00241cb8eb7a83063e2338834
castaglia <castaglia>
2008-04-16 16:26:33 +0000

    Update version for CVS.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030201
-#define PROFTPD_VERSION_TEXT		"1.3.2rc1"
+#define PROFTPD_VERSION_NUMBER		0x0001030202
+#define PROFTPD_VERSION_TEXT		"1.3.2rc2"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
@@ -12,4 +12,4 @@ unsigned long pr_version_get_number(void);
 const char *pr_version_get_str(void);
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(devel)"
+#define PR_STATUS          		"(CVS)"
