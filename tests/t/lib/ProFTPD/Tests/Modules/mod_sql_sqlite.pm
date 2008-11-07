ebfcd535141822636f47faec5993488c071e4475
castaglia <castaglia>
2008-11-07 19:23:10 +0000

    Add a regression test for Bug#2922.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_sql_sqlite.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_sql_sqlite.pm
@@ -16,6 +16,11 @@ $| = 1;
 my $order = 0;
 
 my $TESTS = {
+  sql_bug2922 => {
+    order => ++$order,
+    test_class => [qw(bug forking rootprivs)],
+  },
+
   sql_bug3116 => {
     order => ++$order,
     test_class => [qw(bug forking)],
@@ -59,13 +64,157 @@ sub tear_down {
   eval { rmtree('tmp') };
 };
 
+sub sql_bug2922 {
+  my $self = shift;
+
+  my $config_file = 'tmp/sqlite.conf';
+  my $pid_file = File::Spec->rel2abs('tmp/sqlite.pid');
+  my $scoreboard_file = File::Spec->rel2abs('tmp/sqlite.scoreboard');
+  my $log_file = File::Spec->rel2abs('sqlite.log');
+  my $sql_log_file = File::Spec->rel2abs('sql.log');
+
+  # Bug#2922 occurred because mod_sql would "authenticate" the plaintext
+  # password (if configured to do so) of a user whose info did NOT come from
+  # mod_sql.
+
+  my $db_file = File::Spec->rel2abs('tmp/proftpd.db');
+
+  # Build up sqlite3 command to create users, groups tables and populate them
+  my $db_script = File::Spec->rel2abs('tmp/proftpd.sql');
+
+  if (open(my $fh, "> $db_script")) {
+    print $fh <<EOS;
+CREATE TABLE users (
+  userid TEXT,
+  passwd TEXT,
+  uid INTEGER,
+  gid INTEGER,
+  homedir TEXT, 
+  shell TEXT
+);
+
+CREATE TABLE groups (
+  groupname TEXT,
+  gid INTEGER,
+  members TEXT
+);
+EOS
+
+    unless (close($fh)) {
+      die("Can't write $db_script: $!");
+    }
+
+  } else {
+    die("Can't open $db_script: $!");
+  }
+
+  my $cmd = "sqlite3 $db_file < $db_script";
+
+  if ($ENV{TEST_VERBOSE}) {
+    print STDERR "Executing sqlite3: $cmd\n";
+  }
+
+  my @output = `$cmd`;
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    MaxLoginAttempts => 100,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_sql.c' => {
+        SQLAuthTypes => 'plaintext',
+        SQLBackend => 'sqlite3',
+        SQLConnectInfo => $db_file,
+        SQLLogFile => $sql_log_file,
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      # Bug#2922 would allow a login which should NOT happen.  So if
+      # *any* of the following logins succeed, we have a problem.
+
+      my $accounts = [
+        { user => 'bin', passwd => '*' },
+        { user => 'bin', passwd => 'x' },
+        { user => 'daemon', passwd => '*' },
+        { user => 'daemon', passwd => 'x' },
+        { user => 'mysql', passwd => '*' },
+        { user => 'mysql', passwd => 'x' },
+      ];
+
+      foreach my $account (@$accounts) {
+        my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+        my $user = $account->{user};
+        my $passwd = $account->{passwd};
+
+        eval { $client->login($user, $passwd) };
+        unless ($@) {
+          die("Logged in unexpectedly");
+        }
+      }
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh, 30) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+  unlink($sql_log_file);
+}
+
 sub sql_bug3116 {
   my $self = shift;
 
-  my $config_file = 'tmp/config.conf';
-  my $pid_file = File::Spec->rel2abs('tmp/config.pid');
-  my $scoreboard_file = File::Spec->rel2abs('tmp/config.scoreboard');
-  my $log_file = File::Spec->rel2abs('config.log');
+  my $config_file = 'tmp/sqlite.conf';
+  my $pid_file = File::Spec->rel2abs('tmp/sqlite.pid');
+  my $scoreboard_file = File::Spec->rel2abs('tmp/sqlite.scoreboard');
+  my $log_file = File::Spec->rel2abs('sqlite.log');
   my $sql_log_file = File::Spec->rel2abs('sql.log');
 
   # Bug#3116 occurred because mod_sql was treating percent signs in user
@@ -193,10 +342,10 @@ EOS
 sub sql_bug3124 {
   my $self = shift;
 
-  my $config_file = 'tmp/config.conf';
-  my $pid_file = File::Spec->rel2abs('tmp/config.pid');
-  my $scoreboard_file = File::Spec->rel2abs('tmp/config.scoreboard');
-  my $log_file = File::Spec->rel2abs('config.log');
+  my $config_file = 'tmp/sqlite.conf';
+  my $pid_file = File::Spec->rel2abs('tmp/sqlite.pid');
+  my $scoreboard_file = File::Spec->rel2abs('tmp/sqlite.scoreboard');
+  my $log_file = File::Spec->rel2abs('sqlite.log');
   my $sql_log_file = File::Spec->rel2abs('sql.log');
 
   my $user = 'proftpd';
@@ -323,10 +472,10 @@ EOS
 sub sql_sqlite_bug3126 {
   my $self = shift;
 
-  my $config_file = 'tmp/config.conf';
-  my $pid_file = File::Spec->rel2abs('tmp/config.pid');
-  my $scoreboard_file = File::Spec->rel2abs('tmp/config.scoreboard');
-  my $log_file = File::Spec->rel2abs('config.log');
+  my $config_file = 'tmp/sqlite.conf';
+  my $pid_file = File::Spec->rel2abs('tmp/sqlite.pid');
+  my $scoreboard_file = File::Spec->rel2abs('tmp/sqlite.scoreboard');
+  my $log_file = File::Spec->rel2abs('sqlite.log');
   my $sql_log_file = File::Spec->rel2abs('sql.log');
 
   my $user = 'proftpd';
