a729201779f9e56ad7f9a7190a7249d960381e7d
castaglia <castaglia>
2008-12-04 21:45:58 +0000

    Bug#3144 - mod_dynmasq returns same IP address, even though actual IP address has changed.

--- contrib/mod_dynmasq.c
+++ contrib/mod_dynmasq.c
@@ -26,7 +26,7 @@
  * This is mod_dynmasq, contrib software for proftpd 1.2.x and above.
  * For more information contact TJ Saunders <tj@castaglia.org>.
  *
- * $Id: mod_dynmasq.c,v 1.5 2008-09-30 08:00:10 castaglia Exp $
+ * $Id: mod_dynmasq.c,v 1.6 2008-12-04 21:45:58 castaglia Exp $
  */
 
 #include "conf.h"
@@ -64,6 +64,11 @@ MODRET set_dynmasqrefresh(cmd_rec *cmd) {
 
 static int dynmasq_update_cb(CALLBACK_FRAME) {
   server_rec *s;
+
+  /* Clear the netaddr cache.  Sadly, this is required in order for any
+   * updates to be discovered this way.
+   */
+  pr_netaddr_clear_cache();
 
   pr_log_debug(DEBUG2, MOD_DYNMASQ_VERSION
     ": resolving all MasqueradeAddress directives (could take a little while)");
