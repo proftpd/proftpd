cb86b99b20a7a61d3915fa5b90eadf8855e24cf5
castaglia <castaglia>
2008-12-09 22:14:25 +0000

    Regression tests for Bug#3147.

--- tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
+++ tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
@@ -15,6 +15,8 @@ $| = 1;
 
 my $order = 0;
 
+# A fun collection of various <Directory> and <Limit> configurations
+
 my $TESTS = {
   limits_with_glob_denied_delete_bug3146 => {
     order => ++$order,
@@ -26,6 +28,20 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  limits_commands_comma_space_delimited_deferred_paths_bug3147 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
+  limits_commands_comma_delimited_deferred_paths_bug3147 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
+  limits_commands_no_commas_deferred_paths_bug3147 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
 };
 
 sub new {
@@ -351,6 +367,487 @@ sub limits_without_glob_denied_delete_bug3146 {
         test_msg("Expected $expected, got $resp_code"));
 
       $expected = "test.txt: No such file or directory",
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_commands_comma_space_delimited_deferred_paths_bug3147 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  # This config is from:
+  #
+  #  http://forums.proftpd.org/smf/index.php/topic,3648.0
+  #  http://bugs.proftpd.org/show_bug.cgi?id=3147
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+  my $log_file = File::Spec->rel2abs('dir.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs("$tmpdir/home/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/home/$user/upload");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir, $sub_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir, $sub_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    Directory => {
+      "~$user" => {
+        Limit => {
+          'WRITE' => {
+            DenyAll => '',
+          },
+        },
+      },
+
+      # Bug#3147 happened because proftpd's parsing of the list of commands
+      # in the <Limit> section was not properly handling commas after the
+      # command names.
+
+      "~$user/upload" => {
+        Limit => {
+          'STOR, APPE, MKD, RMD, RNTO, STOU, XMKD, XRMD' => {
+            AllowAll => '',
+          },
+        },
+      },
+    },
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+
+      my $conn = $client->stor_raw('upload/test.txt');
+      unless ($conn) {
+        die("Failed to STOR upload/test.txt: " . $client->response_code() .
+          " " . $client->response_msg());
+      }
+
+      my $buf = "Hello, World!\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_commands_comma_delimited_deferred_paths_bug3147 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  # This config is from:
+  #
+  #  http://forums.proftpd.org/smf/index.php/topic,3648.0
+  #  http://bugs.proftpd.org/show_bug.cgi?id=3147
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+  my $log_file = File::Spec->rel2abs('dir.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs("$tmpdir/home/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/home/$user/upload");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir, $sub_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir, $sub_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    Directory => {
+      "~$user" => {
+        Limit => {
+          'WRITE' => {
+            DenyAll => '',
+          },
+        },
+      },
+
+      # Bug#3147 happened because proftpd's parsing of the list of commands
+      # in the <Limit> section was not properly handling commas after the
+      # command names.
+
+      "~$user/upload" => {
+        Limit => {
+          'STOR,APPE,MKD,RMD,RNTO,STOU,XMKD,XRMD' => {
+            AllowAll => '',
+          },
+        },
+      },
+    },
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+
+      my $conn = $client->stor_raw('upload/test.txt');
+      unless ($conn) {
+        die("Failed to STOR upload/test.txt: " . $client->response_code() .
+          " " . $client->response_msg());
+      }
+
+      my $buf = "Hello, World!\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_commands_no_commas_deferred_paths_bug3147 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  # This config is from:
+  #
+  #  http://forums.proftpd.org/smf/index.php/topic,3648.0
+  #  http://bugs.proftpd.org/show_bug.cgi?id=3147
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+  my $log_file = File::Spec->rel2abs('dir.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs("$tmpdir/home/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/home/$user/upload");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir, $sub_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir, $sub_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    Directory => {
+      "~$user" => {
+        Limit => {
+          'WRITE' => {
+            DenyAll => '',
+          },
+        },
+      },
+
+      # Bug#3147 happened because proftpd's parsing of the list of commands
+      # in the <Limit> section was not properly handling commas after the
+      # command names.  Make sure the fix for Bug#3147 doesn't break command
+      # lists which DO NOT use commas.
+
+      "~$user/upload" => {
+        Limit => {
+          'STOR APPE MKD RMD RNTO STOU XMKD XRMD' => {
+            AllowAll => '',
+          },
+        },
+      },
+    },
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+
+      my $conn = $client->stor_raw('upload/test.txt');
+      unless ($conn) {
+        die("Failed to STOR upload/test.txt: " . $client->response_code() .
+          " " . $client->response_msg());
+      }
+
+      my $buf = "Hello, World!\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
       $self->assert($expected eq $resp_msg,
         test_msg("Expected '$expected', got '$resp_msg'"));
     };
