041656a9b851e1b9469d6cbbbafaa8e98314514f
castaglia <castaglia>
2008-12-10 18:43:39 +0000

    Added unit test for the HiddenStores functionality.  Added more <Limit> cases.

--- tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
+++ tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
@@ -18,6 +18,16 @@ my $order = 0;
 # A fun collection of various <Directory> and <Limit> configurations
 
 my $TESTS = {
+  limits_with_glob_then_nonglob_dirs_for_same_path => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  limits_with_nonglob_then_glob_dirs_for_same_path => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
   limits_with_glob_denied_delete_bug3146 => {
     order => ++$order,
     test_class => [qw(bug forking)],
@@ -74,6 +84,311 @@ sub tear_down {
 
   undef $self;
 };
+
+sub limits_with_glob_then_nonglob_dirs_for_same_path {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+  my $log_file = File::Spec->rel2abs('dir.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs("$tmpdir/home/users/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $abs_tmp_dir = File::Spec->rel2abs($tmpdir);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # In order to ensure that the <Directory> sections appear in the desired
+  # order in the config file, we write them out here.  The config_write()
+  # function, whilst useful, uses hashrefs, which means the ordering of the
+  # keys is not necessarily predictable.
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOD;
+<Directory $abs_tmp_dir/home/*/proftpd>
+  <Limit ALL>
+    DenyAll
+  </Limit>
+</Directory>
+<Directory $abs_tmp_dir/home/users/proftpd>
+  <Limit ALL>
+    AllowAll
+  </Limit>
+</Directory>
+EOD
+    close($fh);
+
+  } else {
+    die("Can't read $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+
+      my $conn = $client->stor_raw('test.txt');
+      unless ($conn) {
+        die("Failed to STOR test.txt: " . $client->response_code() . " " .
+          $client->response_msg());
+      };
+
+      my $buf = "Hello, World\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_with_nonglob_then_glob_dirs_for_same_path {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+  my $log_file = File::Spec->rel2abs('dir.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs("$tmpdir/home/users/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $abs_tmp_dir = File::Spec->rel2abs($tmpdir);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # In order to ensure that the <Directory> sections appear in the desired
+  # order in the config file, we write them out here.  The config_write()
+  # function, whilst useful, uses hashrefs, which means the ordering of the
+  # keys is not necessarily predictable.
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOD;
+<Directory $abs_tmp_dir/home/users/proftpd>
+  <Limit ALL>
+    AllowAll
+  </Limit>
+</Directory>
+<Directory $abs_tmp_dir/home/*/proftpd>
+  <Limit ALL>
+    DenyAll
+  </Limit>
+</Directory>
+EOD
+    close($fh);
+
+  } else {
+    die("Can't read $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+
+      my $conn = $client->stor_raw('test.txt');
+      if ($conn) {
+        die("STOR test.txt succeeded unexpectedly");
+      };
+
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 550;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "test.txt: No such file or directory";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
 
 sub limits_with_glob_denied_delete_bug3146 {
   my $self = shift;
--- tests/tests.pl
+++ tests/tests.pl
@@ -86,6 +86,7 @@ if (scalar(@ARGV) > 0) {
     t/config/displayconnect.t
     t/config/displaylogin.t
     t/config/groupowner.t
+    t/config/hiddenstores.t
     t/config/hidefiles.t
     t/config/maxinstances.t
     t/config/maxloginattempts.t
