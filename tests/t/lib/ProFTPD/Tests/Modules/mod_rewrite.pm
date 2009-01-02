a7417f9effd9f69a8d7ea5e161dfb13ce2a2c98e
castaglia <castaglia>
2009-01-02 19:38:03 +0000

    While investigating Bug#3160, I wrote a mod_rewrite unit test which turned out to be the regression test for Bug#3027 (as Bug#3160 turned out to be a duplicate of Bug#3027).  As such, that test should be added to the collection.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
@@ -36,10 +36,16 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  rewrite_bug3027 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
   rewrite_bug3034 => {
     order => ++$order,
     test_class => [qw(bug forking)],
   },
+
 };
 
 sub new {
@@ -71,7 +77,7 @@ sub tear_down {
   }
 
   undef $self;
-};
+}
 
 sub rewrite_map_lowercase {
   my $self = shift;
@@ -80,7 +86,8 @@ sub rewrite_map_lowercase {
   my $config_file = "$tmpdir/rewrite.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
-  my $log_file = File::Spec->rel2abs('rewrite.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
@@ -223,7 +230,8 @@ sub rewrite_map_underscores {
   my $config_file = "$tmpdir/rewrite.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
-  my $log_file = File::Spec->rel2abs('rewrite.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
@@ -365,7 +373,8 @@ sub rewrite_rule_append_pid {
   my $config_file = "$tmpdir/rewrite.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
-  my $log_file = File::Spec->rel2abs('rewrite.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
@@ -495,7 +504,8 @@ sub rewrite_bug2915 {
   my $config_file = "$tmpdir/rewrite.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
-  my $log_file = File::Spec->rel2abs('rewrite.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
@@ -628,6 +638,149 @@ sub rewrite_bug2915 {
   unlink($log_file);
 }
 
+sub rewrite_bug3027 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/rewrite.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+ 
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_rewrite.c' => [
+        'RewriteEngine on',
+        "RewriteLog $log_file",
+        'RewriteMap replace int:replaceall',
+
+        # Attempt to handle the case where the filename portion of the
+        # uploaded path might be prefaced by spaces.  Bug#3027 occurred
+        # because unmatched backreferences in RewriteRule were not being
+        # substituted properly.
+
+        'RewriteCondition %m ^STOR$',
+        'RewriteRule "^(.*/)? ([^/]*)$" "$1$2"',
+      ],
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      my ($resp_code, $resp_msg);
+
+      $client->login($user, $passwd);
+
+      my $conn = $client->stor_raw(' test.txt');
+
+      my $buf = "Hello, World\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      # Had Bug#3027 been present, the file name would have been "$1test.txt",
+      # not "test.txt".
+      $self->assert(-f $test_file,
+        test_msg("$test_file file does not exist as expected"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
 sub rewrite_bug3034 {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
@@ -635,7 +788,8 @@ sub rewrite_bug3034 {
   my $config_file = "$tmpdir/rewrite.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
-  my $log_file = File::Spec->rel2abs('rewrite.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
