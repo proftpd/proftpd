547d77392b8bb9d3643d08646bf718b0ad237d3b
castaglia <castaglia>
2009-02-14 21:16:16 +0000

    Add separate mod_wrap2 deny table tests for IP addresses and DNS names.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_wrap2_sql.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_wrap2_sql.pm
@@ -36,7 +36,12 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
-  wrap2_sql_deny_table => {
+  wrap2_sql_deny_table_ip_addr => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  wrap2_sql_deny_table_dns_name => {
     order => ++$order,
     test_class => [qw(forking)],
   },
@@ -90,7 +95,8 @@ sub wrap2_allow_msg {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -255,7 +261,8 @@ sub wrap2_deny_msg {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -428,7 +435,8 @@ sub wrap2_engine {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -593,7 +601,8 @@ sub wrap2_sql_allow_table {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -820,14 +829,15 @@ EOS
   unlink($log_file);
 }
 
-sub wrap2_sql_deny_table {
+sub wrap2_sql_deny_table_ip_addr {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
 
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -906,13 +916,13 @@ EOS
         DelayEngine => 'off',
       },
 
-      'mod_sql_sqlite.c' => {
-        SQLAuthenticate => 'off',
-        SQLConnectInfo => $db_file,
-        SQLNamedQuery => 'get-allowed-clients SELECT "allowed FROM ftpallow WHERE name = \'%{0}\'"',
-        SQLNamedQuery => 'get-denied-clients SELECT "denied FROM ftpdeny WHERE name = \'%{0}\'"',
-        SQLLogFile => $log_file,
-      },
+      'mod_sql_sqlite.c' => [
+        "SQLAuthenticate off",
+        "SQLConnectInfo $db_file",
+        'SQLNamedQuery get-allowed-clients SELECT "allowed FROM ftpallow WHERE name = \'%{0}\'"',
+        'SQLNamedQuery get-denied-clients SELECT "denied FROM ftpdeny WHERE name = \'%{0}\'"',
+        "SQLLogFile $log_file",
+      ],
 
       'mod_wrap2_sql.c' => {
         WrapEngine => 'on',
@@ -1072,6 +1082,260 @@ EOS
   unlink($log_file);
 }
 
+sub wrap2_sql_deny_table_dns_name {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/wrap2.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
+
+  my $db_file = File::Spec->rel2abs("$tmpdir/wrap2.db");
+
+  # Build up sqlite3 command to create allow, deny tables and populate them
+  my $db_script = File::Spec->rel2abs("$tmpdir/wrap2.sql");
+
+  my $fh;
+  if (open($fh, "> $db_script")) {
+    print $fh <<EOS;
+CREATE TABLE ftpallow (
+  name TEXT,
+  allowed TEXT
+);
+
+CREATE TABLE ftpdeny (
+  name TEXT,
+  denied TEXT
+);
+
+INSERT INTO ftpdeny (name, denied) VALUES ('', 'ALL');
+EOS
+
+    unless (close($fh)) {
+      die("Can't write $db_script: $!");
+    }
+
+  } else {
+    die("Can't open $db_script: $!");
+  }
+
+  my $cmd = "sqlite3 $db_file < $db_script";
+
+  if ($ENV{TEST_VERBOSE}) {
+    print STDERR "Executing sqlite3: $cmd\n";
+  }
+
+  my @output = `$cmd`;
+
+  unlink($db_script);
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    UseReverseDNS => 'on',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_sql_sqlite.c' => [
+        'SQLAuthenticate off',
+        "SQLConnectInfo $db_file",
+        'SQLNamedQuery get-allowed-clients SELECT "allowed FROM ftpallow WHERE name = \'%{0}\'"',
+        'SQLNamedQuery get-denied-clients SELECT "denied FROM ftpdeny WHERE name = \'%{0}\'"',
+        "SQLLogFile $log_file",
+      ],
+
+      'mod_wrap2_sql.c' => {
+        WrapEngine => 'on',
+        WrapTables => "sql:/get-allowed-clients sql:/get-denied-clients",
+        WrapLog => $log_file,
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      my ($resp_code, $resp_msg);
+
+      eval { $client->login($user, $passwd) };
+      unless ($@) {
+        die("Login succeeded unexpectedly");
+
+      } else {
+        $resp_code = $client->response_code();
+        $resp_msg = $client->response_msg();
+      }
+
+      my $expected;
+
+      $expected = 530;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Access denied";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  if (open($fh, "> $db_script")) {
+    print $fh <<EOS;
+DELETE FROM ftpdeny;
+INSERT INTO ftpdeny (name, denied) VALUES ('', 'localhost');
+EOS
+    unless (close($fh)) {
+      die("Can't write $db_script: $!");
+    }
+
+  } else {
+    die("Can't open $db_script: $!");
+  }
+
+  $cmd = "sqlite3 $db_file < $db_script";
+
+  if ($ENV{TEST_VERBOSE}) {
+    print STDERR "Executing sqlite3: $cmd\n";
+  }
+
+  @output = `$cmd`;
+
+  unlink($db_script);
+
+  ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Fork child
+  $self->handle_sigchld();
+  defined($pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      my ($resp_code, $resp_msg);
+
+      eval { $client->login($user, $passwd) };
+      unless ($@) {
+        die("Login succeeded unexpectedly");
+
+      } else {
+        $resp_code = $client->response_code();
+        $resp_msg = $client->response_msg();
+      }
+
+      my $expected;
+
+      $expected = 530;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Access denied";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
 sub wrap2_sql_user_tables {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
@@ -1079,7 +1343,8 @@ sub wrap2_sql_user_tables {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
@@ -1304,7 +1569,8 @@ sub wrap2_sql_group_tables {
   my $config_file = "$tmpdir/wrap2.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/wrap2.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/wrap2.scoreboard");
-  my $log_file = File::Spec->rel2abs('wrap2.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/wrap2.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/wrap2.group");
