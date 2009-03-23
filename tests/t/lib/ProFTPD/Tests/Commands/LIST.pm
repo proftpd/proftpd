a680e7120181de735ed87f931e017ab84d1b7570
castaglia <castaglia>
2009-03-23 01:12:30 +0000

    Add a unit test for Bug#2821 for the LIST command as well.  It is/can be useful for diagnosing/tuning the performance of the time needed to free all of the struct filename structures allocated for LIST entries (needed for the potential -C sorting).  Further gains can be done by *only allocating* these structures if the -C option is used.
    Add a unit test using LIST -C, also for such tests.

--- tests/t/lib/ProFTPD/Tests/Commands/LIST.pm
+++ tests/t/lib/ProFTPD/Tests/Commands/LIST.pm
@@ -66,6 +66,16 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
+  list_bug2821 => {
+    order => ++$order,
+    test_class => [qw(bug forking slow)],
+  },
+
+  list_opt_C => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
   # XXX Plenty of other tests needed: params, maxfiles, maxdirs, depth, etc
 };
 
@@ -1332,6 +1342,337 @@ sub list_fails_eperm {
       $expected = "$test_file: Permission denied";
       $self->assert($expected eq $resp_msg,
         test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub list_bug2821 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  
+  my $config_file = "$tmpdir/cmds.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  # For this test, we need to create MANY (i.e. 110K) files in the
+  # home directory.
+  my $test_file_prefix = File::Spec->rel2abs("$tmpdir/test_");
+
+  my $count = 110000;
+  print STDOUT "# Creating $count files in $tmpdir\n";
+  for (my $i = 1; $i <= $count; $i++) {
+    my $test_file = 'test_' . sprintf("%07s", $i);
+    my $test_path = "$home_dir/$test_file";
+
+    if (open(my $fh, "> $test_path")) {
+      close($fh);
+
+    } else {
+      die("Can't open $test_path: $!");
+    }
+
+    if ($i % 10000 == 0) {
+      print STDOUT "# Created file $test_file\n";
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $timeout = 900;
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    TimeoutIdle => $timeout + 15,
+    TimeoutNoTransfer => $timeout + 15,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      $client->login($user, $passwd);
+
+      # One of the symptoms of Bug#2821 is that a LIST (or NLST) glob
+      # which matches more than 99999 files will not receive the full
+      # list.
+      #
+      # The issue turned out to be the MAX_RESULTS (now
+      # PR_TUNABLE_GLOBBING_MAX_MATCHES) limit in lib/glibc-glob.c.
+      # So now, we deliberate make sure that the number of files returned
+      # is less than the number of potential matches.
+
+      my $conn = $client->list_raw("-C test_*");
+      unless ($conn) {
+        die("LIST failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf;
+      my $tmp;
+      while ($conn->read($tmp, 32768, $timeout)) {
+        $buf .= $tmp;
+      }
+      $conn->close();
+      $client->quit();
+
+      my $res = {};
+
+      # Preallocate memory for the expected hash size, to help speed up the
+      # evaluation of the results.
+      keys(%$res) = $count;
+
+      my $lines = [split(/\n/, $buf)];
+      foreach my $line (@$lines) {
+        if ($line =~ /^\S+\s+\d+\s+\S+\s+\S+\s+.*?\s+(\S+)$/) {
+          $res->{$line} = 1;
+        }
+      }
+
+      my $list_count = scalar(keys(%$res));
+      if ($list_count >= $count) {
+        die("LIST returned wrong number of entries (expected less than $count, got $list_count); check the PR_TUNABLE_GLOBBING_MAX_MATCHES value");
+      }
+
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh, $timeout * 2) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub list_opt_C {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/cmds.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  # For this test, we need to create about 100 files in home directory.
+  my $test_file_prefix = File::Spec->rel2abs($tmpdir);
+
+  my $count = 100;
+  print STDOUT "# Creating $count files in $tmpdir\n";
+  for (my $i = 1; $i <= $count; $i++) {
+    my $test_file = sprintf("%04s", $i);
+    my $test_path = "$home_dir/$test_file";
+
+    if (open(my $fh, "> $test_path")) {
+      close($fh);
+
+    } else {
+      die("Can't open $test_path: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      $client->login($user, $passwd);
+
+      my $conn = $client->list_raw("-C 0*");
+      unless ($conn) {
+        die("LIST failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf;
+      $conn->read($buf, 32768);
+      $conn->close();
+
+      my $res = {};
+      my $lines = [split(/\n/, $buf)];
+      foreach my $line (@$lines) {
+        if ($line =~ /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$/) {
+          $res->{$1} = 1;
+          $res->{$2} = 1;
+          $res->{$3} = 1;
+          $res->{$4} = 1;
+          $res->{$5} = 1;
+          $res->{$6} = 1;
+          $res->{$7} = 1;
+          $res->{$8} = 1;
+          $res->{$9} = 1;
+
+        } elsif ($line =~ /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$/) {
+          $res->{$1} = 1;
+          $res->{$2} = 1;
+          $res->{$3} = 1;
+          $res->{$4} = 1;
+          $res->{$5} = 1;
+          $res->{$6} = 1;
+          $res->{$7} = 1;
+          $res->{$8} = 1;
+        }
+      }
+
+      my $list_count = scalar(keys(%$res));
+      unless ($list_count == $count) {
+        die("LIST returned wrong number of entries (expected $count, got $list_count)");
+      }
     };
 
     if ($@) {
