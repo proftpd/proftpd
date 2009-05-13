89e3d7ba7677478978570803e4788fb4e895a233
castaglia <castaglia>
2009-05-13 16:31:23 +0000

    Add regression test for the 14-digit version of the SITE UTIME command.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_site_misc.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_site_misc.pm
@@ -36,6 +36,11 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
+  site_misc_utime_with_sec_ok => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
 };
 
 sub new {
@@ -76,7 +81,8 @@ sub site_misc_mkdir_ok {
   my $config_file = "$tmpdir/site.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/site.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/site.scoreboard");
-  my $log_file = File::Spec->rel2abs('site.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/site.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/site.group");
@@ -195,7 +201,8 @@ sub site_misc_rmdir_ok {
   my $config_file = "$tmpdir/site.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/site.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/site.scoreboard");
-  my $log_file = File::Spec->rel2abs('site.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/site.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/site.group");
@@ -340,7 +347,8 @@ sub site_misc_symlink_ok {
   my $config_file = "$tmpdir/site.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/site.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/site.scoreboard");
-  my $log_file = File::Spec->rel2abs('site.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/site.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/site.group");
@@ -458,7 +466,8 @@ sub site_misc_utime_ok {
   my $config_file = "$tmpdir/site.conf";
   my $pid_file = File::Spec->rel2abs("$tmpdir/site.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/site.scoreboard");
-  my $log_file = File::Spec->rel2abs('site.log');
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/site.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/site.group");
@@ -537,6 +546,128 @@ sub site_misc_utime_ok {
       my ($atime, $mtime) = (stat($config_file))[8,9];
 
       $expected = 951380760;
+      $self->assert($expected == $atime,
+        test_msg("Expected $expected, got $atime"));
+      $self->assert($expected == $mtime,
+        test_msg("Expected $expected, got $mtime"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub site_misc_utime_with_sec_ok {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/site.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/site.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/site.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/site.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/site.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+      ($resp_code, $resp_msg) = $client->site('UTIME', '20000224082601',
+        'site.conf');
+
+      my $expected;
+
+      $expected = 200;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "SITE UTIME command successful";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      my ($atime, $mtime) = (stat($config_file))[8,9];
+
+      $expected = 951380761;
       $self->assert($expected == $atime,
         test_msg("Expected $expected, got $atime"));
       $self->assert($expected == $mtime,
