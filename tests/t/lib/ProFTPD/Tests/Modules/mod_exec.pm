c3d3d244ae1e02ad13b2365eaf49420f679789f4
castaglia <castaglia>
2009-07-15 05:59:09 +0000

    Add unit test demonstrating, via mod_exec, a use of the new %{bytes_xfer} variable.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_exec.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_exec.pm
@@ -31,6 +31,11 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
+  exec_on_cmd_var_bytes_xfer => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
   exec_on_exit => {
     order => ++$order,
     test_class => [qw(forking)],
@@ -83,8 +88,6 @@ sub exec_on_connect {
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/exec.scoreboard");
 
   my $log_file = File::Spec->rel2abs('tests.log');
-
-  my $ban_tab = File::Spec->rel2abs("$tmpdir/exec.tab");
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
@@ -209,8 +212,6 @@ sub exec_on_cmd {
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/exec.scoreboard");
 
   my $log_file = File::Spec->rel2abs('tests.log');
-
-  my $ban_tab = File::Spec->rel2abs("$tmpdir/exec.tab");
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
@@ -337,8 +338,6 @@ sub exec_on_cmd_var_total_bytes_xfer {
 
   my $log_file = File::Spec->rel2abs('tests.log');
 
-  my $ban_tab = File::Spec->rel2abs("$tmpdir/exec.tab");
-
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
 
@@ -454,7 +453,7 @@ sub exec_on_cmd_var_total_bytes_xfer {
   unlink($log_file);
 }
 
-sub exec_on_exit {
+sub exec_on_cmd_var_bytes_xfer {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
 
@@ -464,7 +463,154 @@ sub exec_on_exit {
 
   my $log_file = File::Spec->rel2abs('tests.log');
 
-  my $ban_tab = File::Spec->rel2abs("$tmpdir/exec.tab");
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+  
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+    print $fh "ABCD\n" x 128;
+
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  my $cmd_file = File::Spec->rel2abs("$tmpdir/cmd.txt");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_exec.c' => {
+        ExecEngine => 'on',
+        ExecLog => $log_file,
+        ExecTimeout => 1,
+        ExecOnCommand => "RETR /bin/bash -c \"echo %{bytes_xfer} > $cmd_file\"",
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+      
+      my $conn = $client->retr_raw('test.txt');
+      unless ($conn) {
+        die("RETR test.txt failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf;
+      while ($conn->read($buf, 8192)) {
+      }
+      $conn->close();
+
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  if (open(my $fh, "< $cmd_file")) {
+    my $line = <$fh>;
+    close($fh);
+
+    chomp($line);
+
+    my $expected = '\d+';
+
+    $self->assert(qr/$expected/, $line,
+      test_msg("Expected '$expected', got '$line'"));
+
+  } else {
+    die("Can't read $cmd_file: $!");
+  }
+
+  unlink($log_file);
+}
+
+sub exec_on_exit {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/exec.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/exec.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/exec.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
@@ -590,8 +736,6 @@ sub exec_on_error {
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/exec.scoreboard");
 
   my $log_file = File::Spec->rel2abs('tests.log');
-
-  my $ban_tab = File::Spec->rel2abs("$tmpdir/exec.tab");
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
