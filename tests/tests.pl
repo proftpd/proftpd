f13ab888e0bb97c82ea714b43322bd05e1fc8181
castaglia <castaglia>
2009-07-18 19:29:50 +0000

    Add unit test for the UserPassword directive.

--- tests/tests.pl
+++ tests/tests.pl
@@ -116,6 +116,7 @@ if (scalar(@ARGV) > 0) {
     t/config/useralias.t
     t/config/useftpusers.t
     t/config/userowner.t
+    t/config/userpassword.t
     t/config/directory/limits.t
     t/config/directory/umask.t
     t/config/ftpaccess/dele.t
