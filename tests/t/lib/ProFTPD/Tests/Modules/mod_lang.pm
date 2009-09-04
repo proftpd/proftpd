d357c2f84ec53539c6d65994bdaaf99e0e3bd1f7
castaglia <castaglia>
2009-09-04 22:02:21 +0000

    Add unit test proving that the FEAT output does not include UTF8 if the LangEngine directive is set to 'off'.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_lang.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_lang.pm
@@ -16,7 +16,12 @@ $| = 1;
 my $order = 0;
 
 my $TESTS = {
-  lang_feat_ok => {
+  lang_feat_default => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  lang_feat_engine_off => {
     order => ++$order,
     test_class => [qw(forking)],
   },
@@ -84,7 +89,7 @@ sub tear_down {
   undef $self;
 }
 
-sub lang_feat_ok {
+sub lang_feat_default {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
 
@@ -187,6 +192,145 @@ sub lang_feat_ok {
       }
 
       $expected = 2;
+      $self->assert($expected == $seen,
+        test_msg("Expected $expected, got $seen"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub lang_feat_engine_off {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/lang.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/lang.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/lang.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/lang.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/lang.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_lang.c' => {
+        LangEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      $client->feat();
+      my $resp_code = $client->response_code();
+      my $resp_msgs = $client->response_msgs();
+
+      my $expected;
+
+      $expected = 211;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      my $feats = {
+        # None of the following should appear in the FEAT list
+
+        ' UTF8' => 1,
+        ' LANG en_US' => 1,
+        ' LANG en-US' => 1,
+        ' LANG en-US*' => 1,
+        ' LANG en_US.UTF-8' => 1,
+        ' LANG en-US.UTF-8' => 1,
+        ' LANG en-US.UTF-8*' => 1,
+      };
+
+      my $seen = 0;
+
+      my $nfeat = scalar(@$resp_msgs);
+      for (my $i = 0; $i < $nfeat; $i++) {
+        if (defined($feats->{$resp_msgs->[$i]})) {
+          $seen++;
+        }
+      }
+
+      $expected = 0;
       $self->assert($expected == $seen,
         test_msg("Expected $expected, got $seen"));
     };
