991b7f2fff5a4da69a15289ae06dc35157fe7f3f
castaglia <castaglia>
2009-11-18 04:08:37 +0000

    Add regression test demonstrating how to make an undeletable directory using the <Limit> + Filter approach.

--- tests/tests.pl
+++ tests/tests.pl
@@ -134,6 +134,7 @@ if (scalar(@ARGV) > 0) {
     t/config/ftpaccess/merging.t
     t/config/ftpaccess/retr.t
     t/config/limit/anonymous.t
+    t/config/limit/rmd.t
     t/config/limit/xmkd.t
     t/config/limit/filters.t
     t/config/limit/subdirs.t
