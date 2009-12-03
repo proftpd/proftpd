4882b83eced8c2d51862c3159dba77d3c9cb20fa
castaglia <castaglia>
2009-12-03 03:57:43 +0000

    Add unit tests showing that even when using SSL/TLS, resuming downloads of >2GB/4GB files via REST works properly.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
@@ -126,6 +126,16 @@ my $TESTS = {
     test_class => [qw(bug forking mod_ifsession)],
   },
 
+  tls_rest_2gb_last_byte => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  tls_rest_4gb_last_byte => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
 };
 
 sub new {
@@ -3415,6 +3425,404 @@ EOC
         test_msg("Expected '$expected', got '$resp_msg'"));
 
       $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tls_rest_2gb_last_byte {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tls.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tls.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tls.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tls.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tls.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $client_cert = File::Spec->rel2abs('t/etc/modules/mod_tls/client-cert.pem');
+  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
+  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
+
+  # According to:
+  #
+  #  http://trac.filezilla-project.org/ticket/4487
+  #
+  # FileZilla tests whether servers support resuming transfers over 2GB by
+  # (IMHO stupidly) seeking to just before the last byte of the file on the
+  # server, and expecting just a single byte.
+  #
+  # Make sure that proftpd does The Right Thing(tm) in a regression test for
+  # this use case.
+
+  # Create a file that is 2GB plus 24 bytes.
+  my $test_len = (2 ** 31) + 24;
+  my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+
+    # Seek to the 2GB limit, then fill the rest with 'A'
+    unless (seek($fh, (2 ** 31), 0)) {
+       die("Can't seek to 2GB length: $!");
+    }
+
+    print $fh "A" x 24;
+
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  my $tmp_file = File::Spec->rel2abs("$tmpdir/test.tmp");
+  my $tmp_fh;
+  unless (open($tmp_fh, "> $tmp_file")) {
+    die("Can't open $tmp_file: $!");
+  }
+ 
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_tls.c' => {
+        TLSEngine => 'on',
+        TLSLog => $log_file,
+        TLSProtocol => 'SSLv3 TLSv1',
+        TLSRequired => 'on',
+        TLSRSACertificateFile => $cert_file,
+        TLSCACertificateFile => $ca_file,
+        TLSOptions => 'NoSessionReuseRequired',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::FTPSSL;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      # Give the server a chance to start up
+      sleep(2);
+
+      my $client = Net::FTPSSL->new('127.0.0.1',
+        Encryption => 'E',
+        Port => $port,
+      );
+
+      unless ($client) {
+        die("Can't connect to FTPS server: " . IO::Socket::SSL::errstr());
+      }
+
+      unless ($client->login($user, $passwd)) {
+        die("Can't login: " . $client->last_message());
+      }
+
+      unless ($client->binary()) {
+        die("Can't set TYPE to binary: " . $client->last_message());
+      }
+
+      my $rest_len = $test_len - 1;
+
+      unless ($client->quot('REST', $rest_len)) {
+        die("Can't REST $rest_len: " . $client->last_message());
+      }
+
+      my $resp_msg = $client->last_message();
+      my $expected;
+
+      $expected = "350 Restarting at $rest_len. Send STORE or RETRIEVE to initiate transfer";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      unless ($client->get($test_file, $tmp_fh)) {
+        die("Can't RETR $test_file: " . $client->last_message());
+      }
+
+      $resp_msg = $client->last_message();
+      $expected = "226 Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      close($tmp_fh);
+
+      my $buflen = (stat($tmp_file))[7];
+      $expected = 1;
+      $self->assert($expected == $buflen,
+        test_msg("Expected $expected, got $buflen"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tls_rest_4gb_last_byte {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tls.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tls.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tls.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tls.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tls.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $client_cert = File::Spec->rel2abs('t/etc/modules/mod_tls/client-cert.pem');
+  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
+  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
+
+  # According to:
+  #
+  #  http://trac.filezilla-project.org/ticket/4487
+  #
+  # FileZilla tests whether servers support resuming transfers over 4GB by
+  # (IMHO stupidly) seeking to just before the last byte of the file on the
+  # server, and expecting just a single byte.
+  #
+  # Make sure that proftpd does The Right Thing(tm) in a regression test for
+  # this use case.
+
+  # Create a file that is 4GB plus 24 bytes.
+  my $test_len = (2 ** 32) + 24;
+  my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+
+    # Seek to the 4GB limit, then fill the rest with 'A'
+    unless (seek($fh, (2 ** 32), 0)) {
+       die("Can't seek to 4GB length: $!");
+    }
+
+    print $fh "A" x 24;
+
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  my $tmp_file = File::Spec->rel2abs("$tmpdir/test.tmp");
+  my $tmp_fh;
+  unless (open($tmp_fh, "> $tmp_file")) {
+    die("Can't open $tmp_file: $!");
+  }
+ 
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_tls.c' => {
+        TLSEngine => 'on',
+        TLSLog => $log_file,
+        TLSProtocol => 'SSLv3 TLSv1',
+        TLSRequired => 'on',
+        TLSRSACertificateFile => $cert_file,
+        TLSCACertificateFile => $ca_file,
+        TLSOptions => 'NoSessionReuseRequired',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::FTPSSL;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      # Give the server a chance to start up
+      sleep(2);
+
+      my $client = Net::FTPSSL->new('127.0.0.1',
+        Encryption => 'E',
+        Port => $port,
+      );
+
+      unless ($client) {
+        die("Can't connect to FTPS server: " . IO::Socket::SSL::errstr());
+      }
+
+      unless ($client->login($user, $passwd)) {
+        die("Can't login: " . $client->last_message());
+      }
+
+      unless ($client->binary()) {
+        die("Can't set TYPE to binary: " . $client->last_message());
+      }
+
+      my $rest_len = $test_len - 1;
+
+      unless ($client->quot('REST', $rest_len)) {
+        die("Can't REST $rest_len: " . $client->last_message());
+      }
+
+      my $resp_msg = $client->last_message();
+      my $expected;
+
+      $expected = "350 Restarting at $rest_len. Send STORE or RETRIEVE to initiate transfer";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      unless ($client->get($test_file, $tmp_fh)) {
+        die("Can't RETR $test_file: " . $client->last_message());
+      }
+
+      $resp_msg = $client->last_message();
+      $expected = "226 Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      close($tmp_fh);
+
+      my $buflen = (stat($tmp_file))[7];
+      $expected = 1;
+      $self->assert($expected == $buflen,
+        test_msg("Expected $expected, got $buflen"));
     };
 
     if ($@) {
