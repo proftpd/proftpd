ab04b03594400481b04ef9e030d43f5ca098f1f2
castaglia <castaglia>
2010-01-19 00:03:30 +0000

    Add unit tests for mod_sftp_sql.

--- tests/tests.pl
+++ tests/tests.pl
@@ -199,6 +199,11 @@ if (scalar(@ARGV) > 0) {
       test_class => [qw(mod_sftp)],
     },
 
+    't/modules/mod_sftp_sql.t' => {
+      order => ++$order,
+      test_class => [qw(mod_sftp mod_sql_sqlite)],
+    },
+
     't/modules/mod_shaper.t' => {
       order => ++$order,
       test_class => [qw(mod_shaper)],
