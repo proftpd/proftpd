336415cd8cc12cbf0eac504ba7642396fea0725d
castaglia <castaglia>
2010-02-11 22:44:03 +0000

    Add unit test for an <Anonymous> login which uses a <Limit LOGIN> rule based on a DSN name (hint: "UseReverseDNS on" is required for the rule to work as expected).

--- tests/t/lib/ProFTPD/Tests/Config/Limit/Anonymous.pm
+++ tests/t/lib/ProFTPD/Tests/Config/Limit/Anonymous.pm
@@ -31,6 +31,11 @@ my $TESTS = {
     test_class => [qw(forking rootprivs)],
   },
 
+  anon_limit_login_dns_name => {
+    order => ++$order,
+    test_class => [qw(forking rootprivs)],
+  },
+
 };
 
 sub new {
@@ -582,6 +587,145 @@ EOC
       $expected = "testdir: Permission denied";
       $self->assert($expected eq $resp_msg,
         test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub anon_limit_login_dns_name {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/limit.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/limit.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/limit.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/limit.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/limit.group");
+
+  my ($config_user, $config_group) = config_get_identity();
+
+  my $anon_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $anon_dir)) {
+      die("Can't set perms on $anon_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $anon_dir)) {
+      die("Can't set owner of $anon_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $config_user, 'test', $uid, $gid, '/tmp',
+    '/bin/bash');
+  auth_group_write($auth_group_file, $config_group, $gid, $config_user);
+
+  # Test the config mentioned here:
+  #
+  #  http://forums.proftpd.org/smf/index.php/topic,4436.0.html
+  #
+  # Specifically this portion:
+  #
+  #  <Anonymous>
+  #    <Limit LOGIN>
+  #      Allow from <dns-name>
+  #      DenyAll
+  #    </Limit>
+  #  </Anonymous>
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 netacl:10 netaddr:10 dns:10',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    UseReverseDNS => 'on',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $user, $group) = config_write($config_file, $config);
+
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOC;
+<Anonymous $anon_dir>
+  User $config_user
+  Group $config_group
+  UserAlias anonymous $config_user
+  RequireValidShell off
+
+  <Limit LOGIN>
+    Allow from localhost
+    DenyAll
+  </Limit>
+</Anonymous>
+EOC
+    unless (close($fh)) {
+      die("Can't write $config_file: $!");
+    }
+
+  } else {
+    die("Can't open $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      $client->login('anonymous', 'ftp@nospam.org');
     };
 
     if ($@) {
