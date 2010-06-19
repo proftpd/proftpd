70cfc8515373e285d5ff0954fcfaa8b60f33b2d9
castaglia <castaglia>
2010-06-19 18:41:57 +0000

    Add regression test (and correct configuration) for Bug#3166.

--- tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
+++ tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
@@ -4,6 +4,7 @@ use lib qw(t/lib);
 use base qw(Test::Unit::TestCase ProFTPD::TestSuite::Child);
 use strict;
 
+use Cwd;
 use File::Path qw(mkpath rmtree);
 use File::Spec;
 use IO::Handle;
@@ -66,6 +67,11 @@ my $TESTS = {
   limits_one_char_dir_bug3337 => {
     order => ++$order,
     test_class => [qw(bug forking)],
+  },
+
+  limits_symlink_dir_bug3166 => {
+    order => ++$order,
+    test_class => [qw(bug forking rootprivs)],
   },
 
 };
@@ -1674,6 +1680,197 @@ sub limits_one_char_dir_bug3337 {
         test_msg("Expected $expected, got $resp_code"));
 
       $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_symlink_dir_bug3166 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $src_dir = File::Spec->rel2abs("$tmpdir/dispatch/$user");
+  mkpath($src_dir);
+
+  my $dst_dir = File::Spec->rel2abs("$tmpdir/writable/dir1");
+  mkpath($dst_dir);
+
+  # Change to the $src_dir in order to create a symlink with a relative
+  # path (so that it will work in the chroot).
+
+  my $cwd = getcwd();
+  unless (chdir($src_dir)) {
+    die("Can't chdir to $src_dir: $!");
+  }
+
+  unless (symlink("../../writable/dir1", "dir1")) {
+    die("Can't symlink '../../writable/dir1' to 'dir1': $!");
+  }
+
+  unless (chdir($cwd)) {
+    die("Can't chdir to $cwd: $!");
+  }
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $src_dir, $dst_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $src_dir, $dst_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    DefaultRoot => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOD;
+<Directory $src_dir>
+  <Limit ALL>
+    Order allow, deny
+    AllowUser $user
+    DenyAll
+  </Limit>
+</Directory>
+
+<Directory $home_dir/writable>
+  <Limit ALL>
+    Order allow, deny
+    AllowUser admin
+    DenyAll
+  </Limit>
+
+  <Limit DIRS PORT PASV EPRT EPSV>
+    Order allow, deny
+    AllowUser admin
+    AllowUser test
+    DenyAll
+  </Limit>
+</Directory>
+
+<Directory $dst_dir>
+
+  # Bug#3166 was caused by using just ALL in this limit; the parent
+  # directory's limit included the DIRS command group, which took precedence
+  # over ALL.  By adding DIRS to the limit (which includes the test user),
+  # the desired behavior is achieved.
+
+  <Limit ALL DIRS>
+    Order allow, deny
+    AllowUser admin
+    AllowUser $user
+    DenyAll
+  </Limit>
+</Directory>
+EOD
+    close($fh);
+
+  } else {
+    die("Can't read $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      $client->cwd('dispatch');
+      $client->nlst();
+
+      $client->cwd($user);
+      $client->nlst();
+
+      $client->cwd('dir1');
+
+      my ($resp_code, $resp_msg) = $client->pwd();
+
+      my $expected;
+
+      $expected = 257;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = '"/writable/dir1" is the current directory';
       $self->assert($expected eq $resp_msg,
         test_msg("Expected '$expected', got '$resp_msg'"));
     };
