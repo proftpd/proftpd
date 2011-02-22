28204baf1f4e4091ba1eef823a92fc889f8ebdea
castaglia <castaglia>
2011-02-22 03:41:49 +0000

    Adding regression test for MaxCommandRate+mod_sftp+mod_ban config.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_sftp/ban.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_sftp/ban.pm
@@ -9,6 +9,7 @@ use File::Spec;
 use IO::Handle;
 use POSIX qw(:fcntl_h);
 use Socket;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 use ProFTPD::TestSuite::FTP;
 use ProFTPD::TestSuite::Utils qw(:auth :config :features :running :test :testsuite);
@@ -19,6 +20,11 @@ my $order = 0;
 
 my $TESTS = {
   sftp_ban_max_login_attempts => {
+    order => ++$order,
+    test_class => [qw(bug forking mod_ban sftp ssh2)],
+  },
+
+  sftp_ban_maxcmdrate_exceeded => {
     order => ++$order,
     test_class => [qw(bug forking mod_ban sftp ssh2)],
   },
@@ -88,6 +94,7 @@ sub sftp_ban_max_login_attempts {
 
   my $user = 'proftpd';
   my $passwd = 'test';
+  my $group = 'ftpd';
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -106,7 +113,7 @@ sub sftp_ban_max_login_attempts {
 
   auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
     '/bin/bash');
-  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+  auth_group_write($auth_group_file, $group, $gid, $user);
 
   my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_rsa_key');
   my $dsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_dsa_key');
@@ -190,6 +197,182 @@ sub sftp_ban_max_login_attempts {
       }
 
       $ssh2->disconnect();
+
+      # Now try 'password' authentication, which would succeed -- except that
+      # mod_ban should now have a ban in place, and prevent us from doing this.
+      # Note that we need to create a new connection to test the mod_ban
+      # rules.
+
+      $ssh2 = Net::SSH2->new();
+      if ($ssh2->connect('127.0.0.1', $port)) {
+        die("Connecting to SSH2 server succeeded unexpectedly");
+      }
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub sftp_ban_maxcmdrate_exceeded {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/sftp.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/sftp.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/sftp.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/sftp.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/sftp.group");
+
+  my $ban_tab = File::Spec->rel2abs("$tmpdir/sftp-ban.tab");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_rsa_key');
+  my $dsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_dsa_key');
+
+  my $rsa_priv_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/test_rsa_key');  my $rsa_pub_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/test_rsa_key.pub');
+
+  my $max_cmd_rate = 10;
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 ssh2:20 sftp:20 scp:20',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    MaxCommandRate => $max_cmd_rate,
+
+    IfModules => {
+      'mod_ban.c' => {
+        BanEngine => 'on',
+        BanLog => $log_file,
+
+        # This says to ban a client which exceeds the MaxCommandRate
+        # limit once within the last 1 minute will be banned for 10 secs
+        BanOnEvent => 'MaxCommandRate 1/00:01:00 00:00:10',
+
+        BanTable => $ban_tab,
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_sftp.c' => [
+        "SFTPEngine on",
+        "SFTPLog $log_file",
+        "SFTPHostKey $rsa_host_key",
+        "SFTPHostKey $dsa_host_key",
+      ],
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::SSH2;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $ssh2 = Net::SSH2->new();
+
+      sleep(1);
+
+      unless ($ssh2->connect('127.0.0.1', $port)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't connect to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      unless ($ssh2->auth_password($user, $passwd)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't login to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      my $sftp = $ssh2->sftp();
+      unless ($sftp) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't use SFTP on SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      for (my $i = 0; $i < ($max_cmd_rate * 5); $i++) {
+        my $start = [gettimeofday()];
+        unless ($sftp->realpath('.')) {
+          last;
+        }
+
+        my $elapsed = tv_interval($start);
+
+        if ($ENV{TEST_VERBOSE}) {
+          print STDERR "$i: elapsed = $elapsed\n";
+        }
+      }
+
+      eval { $ssh2->disconnect() };
 
       # Now try 'password' authentication, which would succeed -- except that
       # mod_ban should now have a ban in place, and prevent us from doing this.
