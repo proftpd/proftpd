472adfcaf655930ab44a0c3a3738acd4cd28fab9
castaglia <castaglia>
2011-03-21 18:30:57 +0000

    Adding regression tests for Bug#3622.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_delay.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_delay.pm
@@ -26,6 +26,16 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
+  delay_extra_user_cmd_bug3622 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
+  delay_extra_pass_cmd_bug3622 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
 };
 
 sub new {
@@ -270,6 +280,322 @@ sub delay_warm_table {
 
   if ($ex) {
     die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub delay_extra_user_cmd_bug3622 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/delay.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/delay.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/delay.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/delay.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/delay.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $delay_tab = File::Spec->rel2abs("$home_dir/delay.tab");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'delay:20',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayTable => $delay_tab,
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      eval { $client->user($user) };
+      unless ($@) {
+        die("Second USER command succeeded unexpectedly");
+      }
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 503;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = 'You are already logged in';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  # Examine the TraceLog, looking for "unable to load DelayTable" messages.
+  # There shouldn't be any.
+
+  if (open(my $fh, "< $log_file")) {
+    my $ok = 1;
+
+    while (my $line = <$fh>) {
+      chomp($line);
+
+      my $expected = '\[\d+\]\s+<(\S+):(\d+)>: (.*?)$';
+
+      if ($line =~ /$expected/) {
+        my $trace_channel = $1;
+        my $trace_level = $2;
+        my $trace_msg = $3;
+
+        next unless $trace_channel eq 'delay';
+
+        if ($trace_msg =~ /unable to load DelayTable/) {
+          $ok = 0;
+          last;
+        }
+      }
+    }
+
+    close($fh);
+
+    $self->assert($ok, test_msg("Trace messages appeared unexpectedly"));
+
+  } else {
+    die("Can't open $log_file: $!");
+  }
+
+  unlink($log_file);
+}
+
+sub delay_extra_pass_cmd_bug3622 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/delay.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/delay.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/delay.scoreboard");
+
+  my $log_file = File::Spec->rel2abs('tests.log');
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/delay.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/delay.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $delay_tab = File::Spec->rel2abs("$home_dir/delay.tab");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'delay:20',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayTable => $delay_tab,
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      eval { $client->pass($passwd) };
+      unless ($@) {
+        die("Second PASS command succeeded unexpectedly");
+      }
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 503;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = 'You are already logged in';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  # Examine the TraceLog, looking for "unable to load DelayTable" messages.
+  # There shouldn't be any.
+
+  if (open(my $fh, "< $log_file")) {
+    my $ok = 1;
+
+    while (my $line = <$fh>) {
+      chomp($line);
+
+      my $expected = '\[\d+\]\s+<(\S+):(\d+)>: (.*?)$';
+
+      if ($line =~ /$expected/) {
+        my $trace_channel = $1;
+        my $trace_level = $2;
+        my $trace_msg = $3;
+
+        next unless $trace_channel eq 'delay';
+
+        if ($trace_msg =~ /unable to load DelayTable/) {
+          $ok = 0;
+          last;
+        }
+      }
+    }
+
+    close($fh);
+
+    $self->assert($ok, test_msg("Trace messages appeared unexpectedly"));
+
+  } else {
+    die("Can't open $log_file: $!");
   }
 
   unlink($log_file);
