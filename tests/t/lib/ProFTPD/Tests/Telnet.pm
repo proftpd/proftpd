8fe914e4c202ba25ad077b52a6a4dc3e3851fe3f
castaglia <castaglia>
2011-09-21 14:49:11 +0000

    Adding regression test for Bug#3697.

--- tests/t/lib/ProFTPD/Tests/Telnet.pm
+++ tests/t/lib/ProFTPD/Tests/Telnet.pm
@@ -20,6 +20,11 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  telnet_iac_bug3697 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
 };
 
 sub new {
@@ -157,6 +162,152 @@ sub telnet_iac_bug3521 {
       unless ($ex =~ /^command\s+timed/) {
         die("Unexpected exception thrown: $ex");
       }
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub telnet_iac_bug3697 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/telnet.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/telnet.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/telnet.scoreboard");
+
+  my $log_file = File::Spec->rel2abs("tests.log");
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/telnet.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/telnet.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  # To reproduce Bug#3697, we expect a filename with a single \377 (decimal 255)
+  # character in it.
+
+  my $filename = "test";
+  $filename .= chr(Net::Telnet::TELNET_IAC());
+  $filename .= ".txt";
+
+  my $test_file = File::Spec->rel2abs("$tmpdir/$filename");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::Telnet;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      sleep(1);
+
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $name = "test";
+      $name .= chr(Net::Telnet::TELNET_IAC());
+      $name .= chr(Net::Telnet::TELNET_IAC());
+      $name .= ".txt";
+
+      my $conn = $client->stor_raw($name);
+      unless ($conn) {
+        die("STOR failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf = "Foo!\n";
+      $conn->write($buf, length($buf));
+      $conn->close();
+
+      my ($resp_code, $resp_msg);
+      $resp_code = $client->response_code();
+      $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "Transfer complete";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      $self->assert(-f $test_file,
+        test_msg("File $test_file does not exist as expected"));
     };
 
     if ($@) {
