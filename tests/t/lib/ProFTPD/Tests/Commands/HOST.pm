a62a52e0d2f20ee1b59c339ef5183fc9bba982be
castaglia <castaglia>
2011-11-17 23:33:28 +0000

    Might as well update these HOST tests to use testcase-specific logging while I'm changing things.

--- tests/t/lib/ProFTPD/Tests/Commands/HOST.pm
+++ tests/t/lib/ProFTPD/Tests/Commands/HOST.pm
@@ -40,7 +40,7 @@ sub host_ok {
   my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
@@ -145,6 +145,9 @@ sub host_ok {
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
