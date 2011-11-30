c41bf6273dc816d96d2992079ce1587bc3a5cb87
castaglia <castaglia>
2011-11-30 15:24:01 +0000

    Adding mod_rewrite regression test showing how to deal with both spaces and tabs in a filename.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
@@ -23,7 +23,12 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
-  rewrite_map_underscores => {
+  rewrite_map_spaces_underscores => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  rewrite_map_whitespace_underscores => {
     order => ++$order,
     test_class => [qw(forking)],
   },
@@ -271,7 +276,7 @@ sub rewrite_map_lowercase {
   unlink($log_file);
 }
 
-sub rewrite_map_underscores {
+sub rewrite_map_spaces_underscores {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
 
@@ -374,6 +379,158 @@ sub rewrite_map_underscores {
 
       # Send the path with spaces; the rewrite rules should handle it
       ($resp_code, $resp_msg) = $client->size("tmp/test file here.txt");
+
+      my $expected;
+
+      $expected = 213;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = '14';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub rewrite_map_whitespace_underscores {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/rewrite.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+ 
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/tmp");
+  mkpath($sub_dir);
+
+  my $test_file = 'test__file__here.txt';
+  my $test_path = File::Spec->rel2abs("$tmpdir/tmp/$test_file");
+
+  if (open(my $fh, "> $test_path")) {
+    print $fh "Hello, World!\n";
+
+    unless (close($fh)) {
+      die("Can't write $test_path: $!");
+    }
+
+  } else {
+    die("Can't open $test_path: $!");
+  }
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_rewrite.c' => [
+        'RewriteEngine on',
+        "RewriteLog $log_file",
+
+        'RewriteMap replace int:replaceall',
+        'RewriteCondition %m SIZE',
+        'RewriteRule ^(.*) "${replace:!$1! !_}"',
+
+        # Need a separate rule for matching tabs; the internal replaceall
+        # map doesn't understand regex classes like "[:space:]".
+        'RewriteCondition %m SIZE',
+        'RewriteRule ^(.*) "${replace:!$1!	!_}"',
+      ],
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+
+      my ($resp_code, $resp_msg);
+
+      $client->login($user, $passwd);
+      $client->type('binary');
+
+      # Send the path with spaces; the rewrite rules should handle it
+      ($resp_code, $resp_msg) = $client->size("tmp/test  file		here.txt");
 
       my $expected;
 
