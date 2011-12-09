0ea1fc577e0917f837b8562e6e45333d191331ce
castaglia <castaglia>
2011-12-09 01:58:40 +0000

    Updating test cases to use testcase-specific logging. Adding regression test for Bug#3629.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_ifsession.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_ifsession.pm
@@ -51,6 +51,11 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  ifauthenticated_bug3629 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
 };
 
 sub new {
@@ -69,7 +74,7 @@ sub ifuser_allowoverwrite {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -78,6 +83,7 @@ sub ifuser_allowoverwrite {
 
   my $user = 'proftpd';
   my $passwd = 'test';
+  my $group = 'ftpd';
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -116,7 +122,7 @@ sub ifuser_allowoverwrite {
 
   auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
     '/bin/bash');
-  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+  auth_group_write($auth_group_file, $group, $gid, $user);
 
   my $config = {
     PidFile => $pid_file,
@@ -209,6 +215,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -223,7 +232,7 @@ sub ifgroup_dir_allow_mkd_bug3467 {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -231,8 +240,8 @@ sub ifgroup_dir_allow_mkd_bug3467 {
   my $test_file = File::Spec->rel2abs($config_file);
 
   my $user = 'proftpd';
-  my $group = 'ftpd'; 
   my $passwd = 'test';
+  my $group = 'ftpd'; 
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -359,6 +368,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -373,7 +385,7 @@ sub ifuser_dir_allow_mkd_bug3467 {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -381,8 +393,8 @@ sub ifuser_dir_allow_mkd_bug3467 {
   my $test_file = File::Spec->rel2abs($config_file);
 
   my $user = 'proftpd';
-  my $group = 'ftpd'; 
   my $passwd = 'test';
+  my $group = 'ftpd'; 
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -492,6 +504,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -506,7 +521,7 @@ sub ifclass_dir_allow_mkd_bug3467 {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -514,8 +529,8 @@ sub ifclass_dir_allow_mkd_bug3467 {
   my $test_file = File::Spec->rel2abs($config_file);
 
   my $user = 'proftpd';
-  my $group = 'ftpd'; 
   my $passwd = 'test';
+  my $group = 'ftpd'; 
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -629,6 +644,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -643,7 +661,7 @@ sub ifuser_login_deny_by_ip  {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -652,6 +670,7 @@ sub ifuser_login_deny_by_ip  {
 
   my $user = 'proftpd';
   my $passwd = 'test';
+  my $group = 'ftpd';
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -670,7 +689,7 @@ sub ifuser_login_deny_by_ip  {
 
   auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
     '/bin/bash');
-  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+  auth_group_write($auth_group_file, $group, $gid, $user);
 
   my $config = {
     PidFile => $pid_file,
@@ -757,6 +776,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -771,7 +793,7 @@ sub ifgroup_ifclass_login_allowclass_bug3547 {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -900,6 +922,9 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -914,7 +939,7 @@ sub ifuser_regex_login_deny_by_ip_bug3625  {
   my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
@@ -1024,6 +1049,161 @@ EOC
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub ifauthenticated_bug3629 { 
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/ifsess.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
+
+  my $test_file = File::Spec->rel2abs($config_file);
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+    close($fh);
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    AllowOverwrite => 'on',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Append the mod_ifsession config to the end of the config file
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOC;
+<Directory />
+  <Limit WRITE>
+    DenyAll
+  </Limit>
+</Directory>
+
+<IfModule mod_ifsession.c>
+  <IfAuthenticated>
+    <Directory $home_dir>
+      <Limit WRITE>
+        AllowAll
+      </Limit>
+    </Directory>
+  </IfAuthenticated>
+</IfModule>
+EOC
+    unless (close($fh)) {
+      die("Can't write $config_file: $!");
+    }
+
+  } else {
+    die("Can't open $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $conn = $client->stor_raw('test.txt');
+      unless ($conn) {
+        die("Failed to STOR test.txt: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      $conn->close();
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
