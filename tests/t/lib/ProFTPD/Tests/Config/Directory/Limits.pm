88daa98a40acf3756732030fa4939b74644cb412
castaglia <castaglia>
2011-12-10 06:06:59 +0000

    Adding regression test for a <Directory> configuration posted in the forums.

--- tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
+++ tests/t/lib/ProFTPD/Tests/Config/Directory/Limits.pm
@@ -79,6 +79,11 @@ my $TESTS = {
     test_class => [qw(bug forking rootprivs)],
   },
 
+  limits_with_multi_globs_denied_delete => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
 };
 
 sub new {
@@ -2053,6 +2058,170 @@ EOD
       }
 
       $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub limits_with_multi_globs_denied_delete {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  # This config is from:
+  #
+  #  http://forums.proftpd.org/smf/index.php/topic,6807.0.html
+
+  my $config_file = "$tmpdir/dir.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/dir.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/dir.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/dir.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/dir.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $users_dir = File::Spec->rel2abs("$tmpdir/home/users");
+  my $home_dir = File::Spec->rel2abs("$users_dir/$user");
+  my $uid = 500;
+  my $gid = 500;
+
+  mkpath($home_dir);
+
+  my $test_file = File::Spec->rel2abs("$home_dir/hiddenfile_foo.txt");
+  if (open(my $fh, "> $test_file")) {
+    print $fh "Hello, World!\n";
+
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+ 
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $abs_tmp_dir = File::Spec->rel2abs($tmpdir);
+
+  my $glob_expr = "$users_dir/*/hiddenfile_*";
+  if ($^O eq 'darwin') {
+    # MacOSX-specific hack/tweak
+    $glob_expr = '/private' . $glob_expr;
+  }
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'DEFAULT:10 fsio:0 directory:10 lock:0',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    DefaultChdir => '~',
+
+    Directory => {
+      $glob_expr => {
+        Limit => {
+          'ALL' => {
+            DenyAll => '',
+          },
+        },
+      },
+    },
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $filename = 'hiddenfile_foo.txt';
+      eval { $client->dele($filename) };
+      unless ($@) {
+        die("DELE succeeded unexpectedly");
+      }
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 550;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "$filename: Operation not permitted";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
     };
 
     if ($@) {
