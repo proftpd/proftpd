1bf7bc9526aff339b8971dc1c6aae122cbc28cc8
castaglia <castaglia>
2012-03-30 05:11:18 +0000

    This should mention a version of "1.3.5rc1", not "1.3.5".

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 
 /* Application version (in various forms) */
 #define PROFTPD_VERSION_NUMBER		0x0001030501
-#define PROFTPD_VERSION_TEXT		"1.3.5"
+#define PROFTPD_VERSION_TEXT		"1.3.5rc1"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
