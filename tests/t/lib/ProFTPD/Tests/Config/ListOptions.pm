cb4b3a6502cb9886b547066c8d54d22d9fba55ab
castaglia <castaglia>
2012-04-18 15:42:32 +0000

    Adding regression test for the ListOptions maxfiles parameter.

--- tests/t/lib/ProFTPD/Tests/Config/ListOptions.pm
+++ tests/t/lib/ProFTPD/Tests/Config/ListOptions.pm
@@ -51,6 +51,11 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  listoptions_maxfiles => {
+    order => ++$order,
+    test_class => [qw(forking slow)],
+  },
+
 };
 
 sub new {
@@ -1370,6 +1375,179 @@ sub listoptions_nlstonly {
 
   } else {
     eval { server_wait($config_file, $rfh, 30) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub listoptions_maxfiles {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/config.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/config.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/config.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/config.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/config.group");
+  
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash'); 
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  # For this test, we need to create MANY (i.e. 110K) files in the
+  # home directory.
+  my $test_file_prefix = File::Spec->rel2abs("$tmpdir/test_");
+
+  my $max_files = 100000;
+
+  my $count = $max_files + 10000;
+  print STDOUT "# Creating $count files in $tmpdir\n";
+  for (my $i = 1; $i <= $count; $i++) {
+    my $test_file = 'test_' . sprintf("%07s", $i);
+    my $test_path = "$home_dir/$test_file";
+
+    if (open(my $fh, "> $test_path")) {
+      close($fh);
+
+    } else {
+      die("Can't open $test_path: $!");
+    }
+
+    if ($i % 10000 == 0) {
+      print STDOUT "# Created file $test_file\n";
+    }
+  }
+ 
+  my $max_files = 100000;
+  my $timeout = 900;
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    TimeoutIdle => $timeout + 15,
+    TimeoutNoTransfer => $timeout + 15,
+
+    ListOptions => "-al maxfiles $max_files",
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $conn = $client->list_raw();
+      unless ($conn) {
+        die("Failed to LIST: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf;
+
+      my $tmp;
+      while ($conn->read($tmp, 16384, 25)) {
+        $buf .= $tmp;
+      }
+
+      eval { $conn->close() };
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      $self->assert_transfer_ok($resp_code, $resp_msg);
+      $client->quit();
+
+      # We have to be careful of the fact that readdir returns directory
+      # entries in an unordered fashion.
+      my $res = [];
+      my $lines = [split(/\n/, $buf)];
+      foreach my $line (@$lines) {
+        if ($line =~ /^\S+\s+\d+\s+\S+\s+\S+\s+.*?\s+(\S+)$/) {
+          push(@$res, $1);
+        }
+      }
+
+      my $nexpected = $max_files;
+      my $nres = scalar(@$res);
+
+      $self->assert($nexpected == $nres,
+        test_msg("Expected $nexpected items, got $nres"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh, $timeout) };
     if ($@) {
       warn($@);
       exit 1;
