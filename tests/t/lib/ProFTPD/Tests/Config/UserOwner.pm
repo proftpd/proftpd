11aab925e74abfae19c62d5a0bf91b256b650e30
castaglia <castaglia>
2012-11-08 19:18:01 +0000

    Updating UserOwner tests to use testcase-specific logging, and added test for UserOwner+MKD.

--- tests/t/lib/ProFTPD/Tests/Config/UserOwner.pm
+++ tests/t/lib/ProFTPD/Tests/Config/UserOwner.pm
@@ -15,7 +15,12 @@ $| = 1;
 my $order = 0;
 
 my $TESTS = {
-  userowner_ok => {
+  userowner_file => {
+    order => ++$order,
+    test_class => [qw(forking rootprivs)],
+  },
+
+  userowner_dir => {
     order => ++$order,
     test_class => [qw(forking rootprivs)],
   },
@@ -35,7 +40,7 @@ sub list_tests {
   return testsuite_get_runnable_tests($TESTS);
 }
 
-sub userowner_ok {
+sub userowner_file {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
 
@@ -43,13 +48,14 @@ sub userowner_ok {
   my $pid_file = File::Spec->rel2abs("$tmpdir/config.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/config.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/config.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/config.group");
   
   my $user = 'proftpd';
   my $passwd = 'test';
+  my $group = 'ftpd';
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my $uid = 500;
   my $gid = 500;
@@ -73,7 +79,7 @@ sub userowner_ok {
     '/bin/bash'); 
   auth_user_write($auth_user_file, $owner, "none", $owner_uid, $gid, $home_dir,
     '/bin/bash'); 
-  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+  auth_group_write($auth_group_file, $group, $gid, $user);
 
   my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
 
@@ -92,6 +98,10 @@ sub userowner_ok {
     },
 
     IfModules => {
+      'mod_cap.c' => {
+        CapabilitiesEngine => 'off',
+      },
+
       'mod_delay.c' => {
         DelayEngine => 'off',
       },
@@ -175,6 +185,151 @@ sub userowner_ok {
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub userowner_dir {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/config.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/config.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/config.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/config.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/config.group");
+  
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  my $owner = 'proftpd2';
+  my $owner_uid = 1000;
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash'); 
+  auth_user_write($auth_user_file, $owner, "none", $owner_uid, $gid, $home_dir,
+    '/bin/bash'); 
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $test_dir = File::Spec->rel2abs("$tmpdir/test.d");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    Directory => {
+      '~' => {
+        UserOwner => $owner,
+      },
+    },
+
+    IfModules => {
+      'mod_cap.c' => {
+        CapabilitiesEngine => 'off',
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg) = $client->mkd('test.d');
+      my $expected;
+
+      $expected = 257;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $expected = "\"$test_dir\" - Directory successfully created";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected response message '$expected', got '$resp_msg'"));
+
+      $client->quit();
+
+      $self->assert(-d $test_dir,
+        test_msg("$test_dir directory does not exist as expected"));
+
+      my $owning_uid = (stat($test_dir))[4];
+
+      $self->assert($owner_uid == $owning_uid,
+        test_msg("Expected owner UID $owner_uid, got $owning_uid"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
@@ -189,7 +344,7 @@ sub userowner_failed_norootprivs {
   my $pid_file = File::Spec->rel2abs("$tmpdir/config.pid");
   my $scoreboard_file = File::Spec->rel2abs("$tmpdir/config.scoreboard");
 
-  my $log_file = File::Spec->rel2abs('tests.log');
+  my $log_file = test_get_logfile();
 
   my $auth_user_file = File::Spec->rel2abs("$tmpdir/config.passwd");
   my $auth_group_file = File::Spec->rel2abs("$tmpdir/config.group");
@@ -224,6 +379,7 @@ sub userowner_failed_norootprivs {
 
   my $user = 'proftpd';
   my $passwd = 'test';
+  my $group = 'ftpd';
   my $home_dir = File::Spec->rel2abs($tmpdir);
   my ($uid, $gid) = (getpwnam($config_user))[2,3];
 
@@ -231,7 +387,7 @@ sub userowner_failed_norootprivs {
     '/bin/bash'); 
   auth_user_write($auth_user_file, $owner, "none", $owner_uid, $gid, $home_dir,
     '/bin/bash'); 
-  auth_group_write($auth_group_file, 'ftpd', $gid, $user);
+  auth_group_write($auth_group_file, $group, $gid, $user);
 
   # Open pipes, for use between the parent and child processes.  Specifically,
   # the child will indicate when it's done with its test by writing a message
@@ -308,6 +464,9 @@ sub userowner_failed_norootprivs {
   $self->assert_child_ok($pid);
 
   if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
     die($ex);
   }
 
