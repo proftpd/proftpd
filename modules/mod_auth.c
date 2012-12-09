e0186e4550bb021f4e16b7b5529c8e2689a8b384
castaglia <castaglia>
2012-12-09 17:52:20 +0000

    Make sure that if we cannot find the requested USER name, we still fire the "authentication-code" event, for any interested listeners (e.g. mod_snmp).

--- modules/mod_auth.c
+++ modules/mod_auth.c
@@ -25,7 +25,7 @@
  */
 
 /* Authentication module for ProFTPD
- * $Id: mod_auth.c,v 1.307 2012-12-04 19:07:39 castaglia Exp $
+ * $Id: mod_auth.c,v 1.308 2012-12-09 17:52:20 castaglia Exp $
  */
 
 #include "conf.h"
@@ -812,11 +812,15 @@ static int setup_env(pool *p, cmd_rec *cmd, char *user, char *pass) {
 
   pw = pr_auth_getpwnam(p, user);
   if (pw == NULL) {
+    int auth_code = PR_AUTH_NOPWD;
+
     pr_log_auth(PR_LOG_NOTICE,
       "USER %s: no such user found from %s [%s] to %s:%i",
       user, session.c->remote_name,
       pr_netaddr_get_ipstr(session.c->remote_addr),
       pr_netaddr_get_ipstr(session.c->local_addr), session.c->local_port);
+    pr_event_generate("mod_auth.authentication-code", &auth_code); 
+
     goto auth_failure;
   }
 
