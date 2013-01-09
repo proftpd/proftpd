f3c2afcbb3789781e18532d6c5f9cce6b534c0c3
castaglia <castaglia>
2013-01-09 23:52:10 +0000

    Adding regression test for TLSMasqueradeAddress directive; see Bug#3862.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
@@ -272,6 +272,11 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
+  tls_config_tlsmasqueradeaddress_bug3862 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
 };
 
 sub new {
@@ -8049,6 +8054,159 @@ sub tls_opts_multiple_lines_bug3800 {
       unless ($res) {
         die("LIST failed unexpectedly: " . $client->message());
       }
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tls_config_tlsmasqueradeaddress_bug3862 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tls.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tls.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tls.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tls.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tls.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
+  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
+
+  my $masq_addr = '1.2.3.4';
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_tls.c' => [
+        "TLSEngine on",
+        "TLSLog $log_file",
+        "TLSProtocol SSLv3 TLSv1",
+        "TLSRequired on",
+        "TLSRSACertificateFile $cert_file",
+        "TLSCACertificateFile $ca_file",
+
+        "TLSMasqueradeAddress $masq_addr",
+      ],
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::FTPSSL;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      # Give the server a chance to start up
+      sleep(2);
+
+      my $client = Net::FTPSSL->new('127.0.0.1',
+        Encryption => 'E',
+        Port => $port,
+        Croak => 1,
+      );
+
+      unless ($client->login($user, $passwd)) {
+        die("Can't login: " . $client->last_message());
+      }
+
+      unless ($client->quot('PASV')) {
+        die("Can't use PASV: " . $client->last_message());
+      }
+
+      my $resp = $client->last_message();
+
+      if ($resp =~ /^227 Entering Passive Mode \((.*)?,\d+,\d+\)/) {
+        my $resp_addr = $1;
+
+        my $expected = $masq_addr;
+        $expected =~ s/\./,/g;
+
+        $self->assert($expected eq $resp_addr,
+          test_msg("Expected PASV address '$expected', got '$resp_addr'"));
+
+      } else {
+        die("PASV response '$resp' did not match expected pattern");
+      }
+
+      $client->quit();
     };
 
     if ($@) {
