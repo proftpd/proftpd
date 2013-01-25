00a908601f6a9d288131649a32e2fc2e4434b562
castaglia <castaglia>
2013-01-25 01:31:04 +0000

    Adding regression test for Bug#3881.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_ifsession.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_ifsession.pm
@@ -61,6 +61,11 @@ my $TESTS = {
     test_class => [qw(bug forking rootprivs)],
   },
 
+  ifgroup_dir_allow_stor_bug3881 => {
+    order => ++$order,
+    test_class => [qw(bug forking rootprivs)],
+  },
+
 };
 
 sub new {
@@ -93,18 +98,6 @@ sub ifuser_allowoverwrite {
   my $uid = 500;
   my $gid = 500;
 
-  # Make sure that, if we're running as root, that the home directory has
-  # permissions/privs set for the account we create
-  if ($< == 0) {
-    unless (chmod(0755, $home_dir)) {
-      die("Can't set perms on $home_dir to 0755: $!");
-    }
-
-    unless (chown($uid, $gid, $home_dir)) {
-      die("Can't set owner of $home_dir to $uid/$gid: $!");
-    }
-  }
-
   my $ftpaccess_file = File::Spec->rel2abs("$tmpdir/.ftpaccess");
   if (open(my $fh, "> $ftpaccess_file")) {
     print $fh "AllowOverwrite on\n";
@@ -123,6 +116,18 @@ sub ifuser_allowoverwrite {
 
   } else {
     die("Can't open $test_file: $!");
+  }
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $test_file)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $test_file)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
   }
 
   auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
@@ -1100,24 +1105,24 @@ sub ifauthenticated_bug3629 {
   my $uid = 500;
   my $gid = 500;
 
-  # Make sure that, if we're running as root, that the home directory has
-  # permissions/privs set for the account we create
-  if ($< == 0) {
-    unless (chmod(0755, $home_dir)) {
-      die("Can't set perms on $home_dir to 0755: $!");
-    }
-
-    unless (chown($uid, $gid, $home_dir)) {
-      die("Can't set owner of $home_dir to $uid/$gid: $!");
-    }
-  }
-
   my $test_file = File::Spec->rel2abs("$tmpdir/test.txt");
   if (open(my $fh, "> $test_file")) {
     close($fh);
 
   } else {
     die("Can't open $test_file: $!");
+  }
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $test_file)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $test_file)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
   }
 
   auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
@@ -1358,6 +1363,167 @@ EOC
       $expected = "Hello user!";
       $self->assert($expected eq $resp_msg,
         test_msg("Expected response message '$expected', got '$resp_msg'"));
+
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub ifgroup_dir_allow_stor_bug3881 { 
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/ifsess.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/ifsess.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/ifsess.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/ifsess.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/ifsess.group");
+
+  my $test_file = File::Spec->rel2abs($config_file);
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd'; 
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'directory:20 fileperms:10',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    DefaultRoot => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Append the mod_ifsession config to the end of the config file
+  if (open(my $fh, ">> $config_file")) {
+    my $limit_dir = $home_dir;
+    if ($^O eq 'darwin') {
+      # MacOSX hack
+      $limit_dir = ('/private' . $limit_dir);
+    }
+
+    print $fh <<EOC;
+<Directory ~>
+  <Limit WRITE>
+    DenyAll
+  </Limit>
+</Directory>
+
+<IfGroup $group>
+  <Directory $limit_dir>
+    <Limit MKD XMKD RMD XRMD>
+      DenyAll
+    </Limit>
+
+    <Limit WRITE>
+      AllowAll
+    </Limit>
+  </Directory>
+</IfGroup>
+EOC
+    unless (close($fh)) {
+      die("Can't write $config_file: $!");
+    }
+
+  } else {
+    die("Can't open $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $path = 'test.txt';
+      my $conn = $client->stor_raw($path);
+      unless ($conn) {
+        die("Can't STOR $path: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $buf = "Hello, World!\n";
+      $conn->write($buf, length($buf), 25);
+      eval { $conn->close() };
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+      $self->assert_transfer_ok($resp_code, $resp_msg);
 
       $client->quit();
     };
