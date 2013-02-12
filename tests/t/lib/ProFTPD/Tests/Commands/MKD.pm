49333adc17580a81744cc1c9c942f1648136a36e
castaglia <castaglia>
2013-02-12 22:56:55 +0000

    Adding regression test for MKD, making sure that SGID bits are propagated properly for newly-created directories (see Bug#3841).

--- tests/t/lib/ProFTPD/Tests/Commands/MKD.pm
+++ tests/t/lib/ProFTPD/Tests/Commands/MKD.pm
@@ -5,6 +5,7 @@ use base qw(ProFTPD::TestSuite::Child);
 use strict;
 
 use Encode;
+use File::Path qw(mkpath);
 use File::Spec;
 use IO::Handle;
 
@@ -39,6 +40,11 @@ my $TESTS = {
   mkd_chrooted_ok => {
     order => ++$order,
     test_class => [qw(forking rootprivs)],
+  },
+
+  mkd_sgid_ok => {
+    order => ++$order,
+    test_class => [qw(forking)],
   },
 
   mkd_fails_enoent => {
@@ -672,6 +678,144 @@ sub mkd_chrooted_ok {
 
       my $perms = ((stat($sub_dir))[2] & 07777);
       $expected = 0755;
+      $self->assert($expected == $perms,
+        test_msg("Expected perms $expected, got $perms"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub mkd_sgid_ok {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/cmds.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/sub.d");
+  mkpath($sub_dir);
+
+  my $test_dir = File::Spec->rel2abs("$sub_dir/test.d");
+ 
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  # Make sure that our sub directory has the SGID bit set
+  my $mode = oct(2755);
+  unless (chmod($mode, $sub_dir)) {
+    die("Can't set perms on $sub_dir: $!"); 
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    Umask => '022 077',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg);
+      ($resp_code, $resp_msg) = $client->mkd($test_dir);
+
+      my $expected;
+
+      $expected = 257;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "\"$test_dir\" - Directory successfully created";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      $self->assert(-d $test_dir,
+        test_msg("$test_dir directory does not exist as expected"));
+
+      my $perms = ((stat($test_dir))[2] & 07777);
+      $expected = oct(2700);
       $self->assert($expected == $perms,
         test_msg("Expected perms $expected, got $perms"));
     };
