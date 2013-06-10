844b423a5a17c24ee3f4f753a94173d67d75cfaf
castaglia <castaglia>
2013-06-10 16:05:32 +0000

    Bug#3941 - RLimitProcesses causes problems with setuid/setreuid.
    Remove the RLimitProcesses and setrlimit(RLIMIT_NPROC) code; it was causing
    more problems (due to setuid(2) tricks) than it solved.

--- modules/mod_rlimit.c
+++ modules/mod_rlimit.c
@@ -23,7 +23,7 @@
  */
 
 /* Resource limit module
- * $Id: mod_rlimit.c,v 1.5 2013-04-03 16:49:44 castaglia Exp $
+ * $Id: mod_rlimit.c,v 1.6 2013-06-10 16:05:32 castaglia Exp $
  */
 
 #include "conf.h"
@@ -490,30 +490,6 @@ MODRET set_rlimitopenfiles(cmd_rec *cmd) {
 #endif
 }
 
-/* usage: RLimitProcesses on|off */
-MODRET set_rlimitprocesses(cmd_rec *cmd) {
-#if defined(RLIMIT_NPROC)
-  int set_nproc = 0;
-  config_rec *c = NULL;
-
-  CHECK_ARGS(cmd, 1);
-  CHECK_CONF(cmd, CONF_ROOT|CONF_VIRTUAL|CONF_GLOBAL);
-
-  set_nproc = get_boolean(cmd, 1);
-  if (set_nproc == -1) {
-    CONF_ERROR(cmd, "expecting Boolean parameter");
-  }
-
-  c = add_config_param(cmd->argv[0], 1, NULL);
-  c->argv[0] = pcalloc(c->pool, sizeof(int));
-  *((int *) c->argv[0]) = set_nproc;
-
-  return PR_HANDLED(cmd);
-#else
-  CONF_ERROR(cmd, "RLimitProcesses is not supported on this platform");
-#endif
-}
-
 static int rlimit_set_core(int scope) {
   rlim_t current, max;
   int res, xerrno;
@@ -720,42 +696,6 @@ static int rlimit_set_memory(int scope) {
   return 0;
 }
 
-static int rlimit_set_nproc(int scope) {
-  const char *proto;
-  rlim_t current, max;
-  int res, xerrno;
-
-  current = max = 0;
-
-  proto = pr_session_get_protocol(0);
-
-  /* If we are handling an SSH2 session, then we need to allow a higher
-   * nproc limit, in order to properly change UIDs (see Bug#3923).  The
-   * way that RLIMIT_NRPOC works is by setting a per-user limit on the
-   * number of processes.  Calling setuid() effectively does a process
-   * giveway (much like chown(2) on a file to a user other than yourself).
-   * Thus to allow that, we need to allow for nproc to be at least one.
-   */
-  if (strncmp(proto, "ssh2", 5) == 0) {
-    current = max = 1;
-  }
-
-  PRIVS_ROOT
-  res = pr_rlimit_set_nproc(current, max);
-  xerrno = errno;
-  PRIVS_RELINQUISH
-
-  if (res < 0) {
-    pr_log_pri(PR_LOG_ERR, "error setting nproc resource limits: %s",
-      strerror(xerrno));
-
-  } else {
-    pr_log_debug(DEBUG2, "set nproc resource limits for session");
-  }
-
-  return res;
-}
-
 /* Event listeners */
 
 static void rlimit_postparse_ev(const void *event_data, void *user_data) {
@@ -778,46 +718,6 @@ static int rlimit_init(void) {
 }
 
 static int rlimit_sess_init(void) {
-  int set_nproc = TRUE;
-
-  /* Since we're a child process, we do not need to set the core resource
-   * limits; we have inherited the limit from our parent.
-   */
- 
-  if (pr_module_exists("mod_exec.c")) {
-    /* If mod_exec is loaded, we need to check if it is enabled for this
-     * session.  If so, then we should NOT set the RLIMIT_NPROC limit,
-     * as that would prevent mod_exec from working properly.
-     */
-    config_rec *c;
-
-    c = find_config(main_server->conf, CONF_PARAM, "ExecEngine", FALSE);
-    if (c != NULL) {
-      int engine;
-
-      engine = *((int *) c->argv[0]);
-      if (engine == TRUE) {
-        set_nproc = FALSE;
-      }
-    }
-  }
-
-  if (set_nproc) {
-    config_rec *c;
-
-    /* Check whether RLimitProcesses was used to disable the default setting
-     * of the nproc limit.
-     */
-    c = find_config(main_server->conf, CONF_PARAM, "RLimitProcesses", FALSE);
-    if (c != NULL) {
-      set_nproc = *((int *) c->argv[0]);
-    }
-  }
-
-  if (set_nproc) { 
-    rlimit_set_nproc(SESSION_SCOPE);
-  }
-
   rlimit_set_cpu(SESSION_SCOPE);
   rlimit_set_memory(SESSION_SCOPE);
   rlimit_set_files(SESSION_SCOPE);
@@ -832,7 +732,6 @@ static conftable rlimit_conftab[] = {
   { "RLimitCPU",		set_rlimitcpu,			NULL },
   { "RLimitMemory",		set_rlimitmemory,		NULL },
   { "RLimitOpenFiles",		set_rlimitopenfiles,		NULL },
-  { "RLimitProcesses",		set_rlimitprocesses,		NULL },
 
   { NULL, NULL, NULL }
 };
