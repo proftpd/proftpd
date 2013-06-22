c0d4fa0d377d2c8eacdaa737abdf3582341b39cb
castaglia <castaglia>
2013-06-22 06:20:18 +0000

    Add a unit test for the pr_fs_dircat() function; we're going to be refactoring it a bit.

--- tests/api/fsio.c
+++ tests/api/fsio.c
@@ -1,6 +1,6 @@
 /*
  * ProFTPD - FTP server testsuite
- * Copyright (c) 2008-2012 The ProFTPD Project team
+ * Copyright (c) 2008-2013 The ProFTPD Project team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
  */
 
 /* FSIO API tests
- * $Id: fsio.c,v 1.1 2012-10-01 21:26:32 castaglia Exp $
+ * $Id: fsio.c,v 1.2 2013-06-22 06:20:18 castaglia Exp $
  */
 
 #include "tests.h"
@@ -67,6 +67,69 @@ START_TEST (fs_clean_path_test) {
 }
 END_TEST
 
+START_TEST (fs_dircat_test) {
+  char buf[PR_TUNABLE_PATH_MAX+1], *a, *b, *ok;
+  int res;
+
+  res = pr_fs_dircat(NULL, 0, NULL, NULL);
+  fail_unless(res == -1, "Failed to handle null arguments");
+  fail_unless(errno == EINVAL,
+    "Failed to set errno to EINVAL for null arguments");
+
+  res = pr_fs_dircat(buf, 0, "foo", "bar");
+  fail_unless(res == -1, "Failed to handle zero-length buffer");
+  fail_unless(errno == EINVAL,
+    "Failed to set errno to EINVAL for zero-length buffer");
+
+  res = pr_fs_dircat(buf, -1, "foo", "bar");
+  fail_unless(res == -1, "Failed to handle negative-length buffer");
+  fail_unless(errno == EINVAL,
+    "Failed to set errno to EINVAL for negative-length buffer");
+
+  a = pcalloc(p, PR_TUNABLE_PATH_MAX);
+  memset(a, 'A', PR_TUNABLE_PATH_MAX-1);
+
+  b = "foo";
+
+  res = pr_fs_dircat(buf, sizeof(buf)-1, a, b);
+  fail_unless(res == -1, "Failed to handle too-long paths");
+  fail_unless(errno == ENAMETOOLONG,
+    "Failed to set errno to ENAMETOOLONG for too-long paths");
+
+  a = "foo";
+  b = "/bar";
+  ok = b;
+  res = pr_fs_dircat(buf, sizeof(buf)-1, a, b);
+  fail_unless(res == 0, "Failed to concatenate abs-path path second dir");
+  fail_unless(strcmp(buf, ok) == 0, "Expected concatenated dir '%s', got '%s'",
+    ok, buf);
+ 
+  a = "foo";
+  b = "bar";
+  ok = "foo/bar";
+  res = pr_fs_dircat(buf, sizeof(buf)-1, a, b);
+  fail_unless(res == 0, "Failed to concatenate two normal paths");
+  fail_unless(strcmp(buf, ok) == 0, "Expected concatenated dir '%s', got '%s'",
+    ok, buf);
+ 
+  a = "foo/";
+  b = "bar";
+  ok = "foo/bar";
+  res = pr_fs_dircat(buf, sizeof(buf)-1, a, b);
+  fail_unless(res == 0, "Failed to concatenate first dir with trailing slash");
+  fail_unless(strcmp(buf, ok) == 0, "Expected concatenated dir '%s', got '%s'",
+    ok, buf);
+
+  a = "";
+  b = "";
+  ok = "/";
+  res = pr_fs_dircat(buf, sizeof(buf)-1, a, b);
+  fail_unless(res == 0, "Failed to concatenate two empty paths");
+  fail_unless(strcmp(buf, ok) == 0, "Expected concatenated dir '%s', got '%s'",
+    ok, buf);
+}
+END_TEST
+
 Suite *tests_get_fsio_suite(void) {
   Suite *suite;
   TCase *testcase;
@@ -78,6 +141,7 @@ Suite *tests_get_fsio_suite(void) {
   tcase_add_checked_fixture(testcase, set_up, tear_down);
 
   tcase_add_test(testcase, fs_clean_path_test);
+  tcase_add_test(testcase, fs_dircat_test);
 
   suite_add_tcase(suite, testcase);
   return suite;
