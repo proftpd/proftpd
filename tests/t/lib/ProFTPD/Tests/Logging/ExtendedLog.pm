347fc4e1f659187a12adfbcfcf2319ad65843755
castaglia <castaglia>
2013-09-18 18:49:28 +0000

    Add ExtendedLog test for the %r LogFormat variable, for commands other than PASS.  Fixed broken mod_sftp test.

--- tests/t/lib/ProFTPD/Tests/Logging/ExtendedLog.pm
+++ tests/t/lib/ProFTPD/Tests/Logging/ExtendedLog.pm
@@ -236,6 +236,11 @@ my $TESTS = {
     test_class => [qw(bug forking mod_sftp ssh2)],
   },
 
+  extlog_var_r => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
   extlog_pass_var_r => {
     order => ++$order,
     test_class => [qw(forking)],
@@ -1199,8 +1204,8 @@ sub extlog_sftp_mlsd_var_d_D_f_F_bug3950 {
         $self->assert($sub_dir eq $dir_path,
           test_msg("Expected '$sub_dir', got '$dir_path'"));
 
-        $self->assert('-' eq $file_name,
-          test_msg("Expected '-', got '$file_name'"));
+        $self->assert($sub_dir eq $file_name,
+          test_msg("Expected '$sub_dir', got '$file_name'"));
 
         $self->assert('-' eq $file_path,
           test_msg("Expected '-', got '$file_path'"));
@@ -7444,6 +7449,169 @@ sub extlog_sftp_xfer_timeout_bug3696 {
 
   } else {
     die("Can't read $ext_log: $!");
+  }
+
+  unlink($log_file);
+}
+
+sub extlog_var_r {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/extlog.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/extlog.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/extlog.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/extlog.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/extlog.group");
+
+  my $test_file = File::Spec->rel2abs($config_file);
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $ext_log = File::Spec->rel2abs("$tmpdir/custom.log");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+    TraceLog => $log_file,
+    Trace => 'response:10',
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    LogFormat => 'custom "%r"',
+    ExtendedLog => "$ext_log ALL custom",
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+      eval { $client->site('CHMOD', '777', 'test.txt') };
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  # Now, read in the ExtendedLog, and see whether the %s variable was
+  # properly written out for the PASS command.
+  eval {
+    if (open(my $fh, "< $ext_log")) {
+      my $user_ok = 0;
+      my $pass_ok = 0;
+      my $site_ok = 0;
+
+      while (my $line = <$fh>) {
+        chomp($line);
+
+        if ($line =~ /^(.*?)$/) {
+          my $cmd = $1;
+
+          if ($cmd eq "USER $user") {
+            $user_ok = 1;
+            next;
+          }
+
+          if ($cmd eq 'PASS (hidden)') {
+            $pass_ok = 1;
+            next;
+          }
+
+          if ($cmd eq 'SITE CHMOD 777 test.txt') {
+            $site_ok = 1;
+            next;
+          }
+        }
+      }
+
+      close($fh);
+
+      $self->assert($user_ok,
+        test_msg("Did not see expected USER command in $ext_log"));
+      $self->assert($pass_ok,
+        test_msg("Did not see expected PASS command in $ext_log"));
+      $self->assert($site_ok,
+        test_msg("Did not see expected SITE command in $ext_log"));
+
+    } else {
+      die("Can't read $ext_log: $!");
+    }
+  };
+  if ($@) {
+    $ex = $@;
+  }
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
   }
 
   unlink($log_file);
