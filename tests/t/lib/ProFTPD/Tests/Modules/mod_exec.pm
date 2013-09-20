a7d0b0514f9c96f76f71ae76dd52ba99173fa4e8
castaglia <castaglia>
2013-09-20 00:52:20 +0000

    Adding regression test for Bug#3964.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_exec.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_exec.pm
@@ -91,6 +91,11 @@ my $TESTS = {
     test_class => [qw(bug forking os_linux)],
   },
 
+  exec_on_event_as_user_bug3964 => {
+    order => ++$order,
+    test_class => [qw(forking rootprivs)],
+  },
+
 };
 
 sub new {
@@ -2244,6 +2249,161 @@ EOS
 
   } else {
     die("Can't read $log_file: $!");
+  }
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub exec_on_event_as_user_bug3964 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/exec.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/exec.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/exec.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/exec.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/exec.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+  
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $chroot_file = File::Spec->rel2abs("$tmpdir/chroot.txt");
+
+  my $script_file = File::Spec->rel2abs("$tmpdir/chroot.sh");
+  if (open(my $fh, "> $script_file")) {
+    print $fh <<EOS;
+#!/bin/bash
+
+echo \$EUID > $chroot_file
+exit 0
+EOS
+    unless (close($fh)) {
+      die("Can't write $script_file: $!");
+    }
+
+  } else {
+    die("Can't open $script_file: $!");
+  }
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    DefaultRoot => '~',
+
+    IfModules => {
+      'mod_exec.c' => {
+        ExecEngine => 'on',
+        ExecLog => $log_file,
+        ExecTimeout => 1,
+        ExecOnEvent => "core.chroot~ /bin/bash $script_file",
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  eval {
+    # MacOSX hack
+    if ($^O eq 'darwin') {
+      $chroot_file = ('/private' . $chroot_file);
+    }
+
+    if (open(my $fh, "< $chroot_file")) {
+      my $line = <$fh>;
+      close($fh);
+      chomp($line);
+
+      my $expected = $uid;
+
+      $self->assert($expected eq $line,
+        test_msg("Expected UID '$expected', got '$line'"));
+
+    } else {
+      die("Can't read $chroot_file: $!");
+    }
+  };
+  if ($@) {
+    $ex = $@;
   }
 
   if ($ex) {
