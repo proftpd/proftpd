7dc04e98d765addb373b5bf4989dd01148921a74
castaglia <castaglia>
2013-12-12 05:48:05 +0000

    Regression tests for Bug#3996.

--- tests/t/lib/ProFTPD/Tests/Commands/ALLO.pm
+++ tests/t/lib/ProFTPD/Tests/Commands/ALLO.pm
@@ -20,9 +20,14 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
-  allo_chrooted_ok => {
+  allo_chrooted_bug3996 => {
     order => ++$order,
-    test_class => [qw(forking rootprivs)],
+    test_class => [qw(bug forking rootprivs)],
+  },
+
+  allo_anon_bug3996 => {
+    order => ++$order,
+    test_class => [qw(bug forking rootprivs)],
   },
 
 };
@@ -121,7 +126,7 @@ sub allo_ok {
   unlink($setup->{log_file});
 }
 
-sub allo_chrooted_ok {
+sub allo_chrooted_bug3996 {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
   my $setup = test_setup($tmpdir, 'cmds');
@@ -167,11 +172,11 @@ sub allo_chrooted_ok {
 
       my $expected;
 
-      $expected = 200;
+      $expected = 202;
       $self->assert($expected == $resp_code,
         test_msg("Expected response code $expected, got $resp_code"));
 
-      $expected = "ALLO command successful";
+      $expected = "No storage allocation necessary";
       $self->assert($expected eq $resp_msg,
         test_msg("Expected response message '$expected', got '$resp_msg'"));
     };
@@ -206,6 +211,138 @@ sub allo_chrooted_ok {
   }
 
   unlink($setup->{log_file});
+}
+
+sub allo_anon_bug3996 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/cmds.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
+
+  my ($config_user, $config_group) = config_get_identity();
+
+  my $anon_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $anon_dir)) {
+      die("Can't set perms on $anon_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $anon_dir)) {
+      die("Can't set owner of $anon_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $config_user, 'test', $uid, $gid, '/tmp',
+    '/bin/bash');
+  auth_group_write($auth_group_file, $config_group, $gid, $config_user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  if (open(my $fh, ">> $config_file")) {
+    print $fh <<EOC;
+<Anonymous $anon_dir>
+  User $config_user
+  Group $config_group
+  UserAlias anonymous $config_user
+  RequireValidShell off
+</Anonymous>
+EOC
+    unless (close($fh)) {
+      die("Can't write $config_file: $!");
+    }
+
+  } else {
+    die("Can't open $config_file: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login('anonymous', 'ftp@nospam.org');
+
+      my ($resp_code, $resp_msg) = $client->allo(8192);
+
+      my $expected;
+
+      $expected = 202;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $expected = "No storage allocation necessary";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected response message '$expected', got '$resp_msg'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
 }
 
 1;
