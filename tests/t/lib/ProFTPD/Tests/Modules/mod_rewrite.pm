7e90c11858e60d5ae7147258a219a4f72700cea4
castaglia <castaglia>
2014-02-05 00:17:56 +0000

    Additional regression/reproduction recipe for Bug#4017, this time using PCRE regexes.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_rewrite.pm
@@ -203,6 +203,11 @@ my $TESTS = {
     test_class => [qw(bug forking rootprivs)],
   },
 
+  rewrite_using_pcre_bug4017 => {
+    order => ++$order,
+    test_class => [qw(bug feature_pcre forking rootprivs)],
+  },
+
 };
 
 sub new {
@@ -5680,6 +5685,138 @@ sub rewrite_bug4017 {
         'RewriteCondition %m !PASS',
         'RewriteCondition %m !USER',
         'RewriteRule (^/[^\\\\]*\\\\) /$1',
+
+        'RewriteCondition %m !PASS',
+        'RewriteCondition %m !USER',
+        'RewriteRule (.*) ${replace:!$1!\\!/}',
+      ],
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my ($resp_code, $resp_msg) = $client->stat("/foo.d/\\bar.d/\\baz.d");
+
+      my $expected;
+
+      $expected = 211;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $client->quit();
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub rewrite_using_pcre_bug4017 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/rewrite.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/rewrite.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/rewrite.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/rewrite.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/rewrite.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $test_dir = File::Spec->rel2abs("$tmpdir/foo.d");
+  mkpath($test_dir);
+ 
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    DefaultRoot => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_rewrite.c' => [
+        'RewriteEngine on',
+        "RewriteLog $log_file",
+        'RewriteMap replace int:replaceall',
+
+        'RewriteCondition %m !PASS',
+        'RewriteCondition %m !USER',
+        'RewriteRule ^(.*?\\/\\\\)(.*) /$1',
 
         'RewriteCondition %m !PASS',
         'RewriteCondition %m !USER',
