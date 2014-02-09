b7c4012ee5800935e94ca9b0c883814b811dc84c
castaglia <castaglia>
2014-02-09 19:41:06 +0000

    Another MKD test, making sure that using a preceding CWD doesn't gum up the works.  Also makes for a nice testbed for pr_fsio_smkdir() issues.

--- tests/t/lib/ProFTPD/Tests/Commands/MKD.pm
+++ tests/t/lib/ProFTPD/Tests/Commands/MKD.pm
@@ -47,6 +47,11 @@ my $TESTS = {
     test_class => [qw(forking rootprivs)],
   },
 
+  mkd_chrooted_with_cwd_ok => {
+    order => ++$order,
+    test_class => [qw(forking rootprivs)],
+  },
+
   mkd_sgid_umask_one_param_ok => {
     order => ++$order,
     test_class => [qw(forking)],
@@ -853,6 +858,137 @@ sub mkd_chrooted_ok {
   unlink($log_file);
 }
 
+sub mkd_chrooted_with_cwd_ok {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/cmds.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/cmds.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/cmds.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/cmds.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/cmds.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/foo");
+  mkpath($sub_dir);
+
+  my $test_dir = File::Spec->rel2abs("$tmpdir/foo/bar");
+ 
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir, $sub_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir, $sub_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+ 
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+    DefaultRoot => '~',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+      $client->cwd('foo');
+
+      my ($resp_code, $resp_msg) = $client->xmkd('bar');
+
+      my $expected;
+
+      $expected = 257;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected $expected, got $resp_code"));
+
+      $expected = "\"/foo/bar\" - Directory successfully created";
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected '$expected', got '$resp_msg'"));
+
+      $self->assert(-d $test_dir,
+        test_msg("$test_dir directory does not exist as expected"));
+
+      my $perms = ((stat($sub_dir))[2] & 07777);
+      $expected = 0755;
+      $self->assert($expected == $perms,
+        test_msg("Expected perms $expected, got $perms"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
 sub mkd_sgid_umask_one_param_ok {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
@@ -1601,6 +1737,11 @@ sub mkd_digits_ok {
       $expected = 257;
       $self->assert($expected == $resp_code,
         test_msg("Expected $expected, got $resp_code"));
+
+      if ($^O eq 'darwin') {
+        # MacOSX hack
+        $sub_dir = '/private' . $sub_dir;
+      }
 
       $expected = "\"$sub_dir\" - Directory successfully created";
       $self->assert($expected eq $resp_msg,
