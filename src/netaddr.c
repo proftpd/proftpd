03aa76e1b15908a509849249e83b0979558b4537
TJ Saunders <tj@castaglia.org>
2014-05-17 17:59:10 -0700

    Avoid null pointer dereference for interfaces with no addresses.

--- src/netaddr.c
+++ src/netaddr.c
@@ -1,6 +1,6 @@
 /*
  * ProFTPD - FTP server daemon
- * Copyright (c) 2003-2013 The ProFTPD Project team
+ * Copyright (c) 2003-2014 The ProFTPD Project team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -761,6 +761,14 @@ static pr_netaddr_t *get_addr_by_device(pool *p, const char *name,
     int found_device = FALSE;
 
     for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+      pr_signals_handle();
+
+      /* Watch out for null ifa_addr, as when a device does not have
+       * an associated address (e.g. due to not be initialized).
+       */
+      if (ifa->ifa_addr == NULL) {
+        continue;
+      }
 
       /* We're only looking for addresses, not stats. */
       if (ifa->ifa_addr->sa_family != AF_INET
