89a9c6c4ac2bdaca5bf7e3d07d01cce4439586f6
tjsaunders <tjsaunders@blackpearlsystems.com>
2014-10-18 22:18:33 -0700

    Handle possible lseek(2) error when scrubbing scoreboard via ftpscrub.

--- utils/scoreboard.c
+++ utils/scoreboard.c
@@ -303,6 +303,9 @@ int util_scoreboard_scrub(int verbose) {
 
   /* Skip past the scoreboard header. */
   curr_offset = lseek(fd, (off_t) sizeof(pr_scoreboard_header_t), SEEK_SET);
+  if (curr_offset < 0) {
+    return -1;
+  }
 
   memset(&sce, 0, sizeof(sce));
 
