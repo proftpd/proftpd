61780f639764e0ab2f34c6bd6c81f34c832578cd
tjsaunders <tj@lyveminds.com>
2015-04-08 14:51:35 -0700

    Remove unnecessary NULL check, per Coverity.

--- src/fsio.c
+++ src/fsio.c
@@ -2973,16 +2973,17 @@ int pr_fsio_chdir_canon(const char *path, int hidesymlink) {
   /* Find the first non-NULL custom chdir handler.  If there are none,
    * use the system chdir.
    */
-  while (fs && fs->fs_next && !fs->chdir)
+  while (fs && fs->fs_next && !fs->chdir) {
     fs = fs->fs_next;
+  }
 
   pr_trace_msg(trace_channel, 8, "using %s chdir() for path '%s'", fs->fs_name,
     path);
   res = (fs->chdir)(fs, resbuf);
 
-  if (res != -1) {
+  if (res == 0) {
     /* chdir succeeded, so we set fs_cwd for future references. */
-     fs_cwd = fs ? fs : root_fs;
+     fs_cwd = fs;
 
      if (hidesymlink) {
        pr_fs_virtual_path(path, vwd, sizeof(vwd)-1);
