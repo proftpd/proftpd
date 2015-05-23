6458975d967be6e070078d388a08a85a4424aa79
tjsaunders <tj@lyveminds.com>
2015-05-22 19:32:39 -0700

    Attempt to address Debian Bug#717235:   https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=717235

--- modules/mod_auth.c
+++ modules/mod_auth.c
@@ -875,16 +875,23 @@ static struct passwd *passwd_dup(pool *p, struct passwd *pw) {
 }
 
 static void ensure_open_passwd(pool *p) {
-  /* Make sure pass/group is open.
-   */
+  /* Make sure pass/group is open. */
   pr_auth_setpwent(p);
   pr_auth_setgrent(p);
 
   /* On some unices the following is necessary to ensure the files
-   * are open.  (BSDI 3.1)
+   * are open (BSDI 3.1)
    */
   pr_auth_getpwent(p);
   pr_auth_getgrent(p);
+
+  /* Per Debian bug report:
+   *   https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=717235
+   * we might want to do another set{pw,gr}ent(), to play better with
+   * some NSS modules.
+   */
+  pr_auth_setpwent(p);
+  pr_auth_setgrent(p);
 }
 
 /* Next function (the biggie) handles all authentication, setting
