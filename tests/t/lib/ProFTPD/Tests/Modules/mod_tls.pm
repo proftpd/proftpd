e566d358dc249f2af2d7252f891b83ba656d4616
TJ Saunders <tj@castaglia.org>
2015-12-22 10:25:46 -0800

    Split out the mod_tls_fscache-related regression tests into their own test file, just as was done for mod_tls_memcache and mod_tls_shmcache.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_tls.pm
@@ -339,11 +339,6 @@ my $TESTS = {
     test_class => [qw(bug forking)],
   },
 
-  tls_stapling_on_fscache_bug4175 => {
-    order => ++$order,
-    test_class => [qw(bug forking mod_tls_fscache)],
-  },
-
 };
 
 sub new {
@@ -10082,7 +10077,7 @@ sub tls_stapling_on_bug4175 {
     ScoreboardFile => $setup->{scoreboard_file},
     SystemLog => $setup->{log_file},
     TraceLog => $setup->{log_file},
-    Trace => 'tls:20 tls.fscache:20',
+    Trace => 'tls:20',
 
     AuthUserFile => $setup->{auth_user_file},
     AuthGroupFile => $setup->{auth_group_file},
@@ -10260,118 +10255,6 @@ sub starttls_ftp {
   print $client $cmd;
   $client->flush();
   $client->close();
-}
-
-sub tls_stapling_on_fscache_bug4175 {
-  my $self = shift;
-  my $tmpdir = $self->{tmpdir};
-  my $setup = test_setup($tmpdir, 'tls');
-
-  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
-  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
-
-  my $cache_tab = File::Spec->rel2abs("$tmpdir/var/tls/cache/ocsp");
-  mkpath($cache_tab);
-
-  my $config = {
-    PidFile => $setup->{pid_file},
-    ScoreboardFile => $setup->{scoreboard_file},
-    SystemLog => $setup->{log_file},
-    TraceLog => $setup->{log_file},
-    Trace => 'tls:20 tls.fscache:20',
-
-    AuthUserFile => $setup->{auth_user_file},
-    AuthGroupFile => $setup->{auth_group_file},
-
-    IfModules => {
-      'mod_delay.c' => {
-        DelayEngine => 'off',
-      },
-
-      'mod_tls.c' => {
-        TLSEngine => 'on',
-        TLSLog => $setup->{log_file},
-        TLSProtocol => 'SSLv3 TLSv1',
-        TLSRequired => 'on',
-        TLSRSACertificateFile => $cert_file,
-        TLSCACertificateFile => $ca_file,
-        TLSOptions => 'EnableDiags',
-        TLSStapling => 'on',
-        TLSStaplingCache => "fs:/path=$cache_tab",
-      },
-    },
-  };
-
-  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
-    $config);
-
-  # Open pipes, for use between the parent and child processes.  Specifically,
-  # the child will indicate when it's done with its test by writing a message
-  # to the parent.
-  my ($rfh, $wfh);
-  unless (pipe($rfh, $wfh)) {
-    die("Can't open pipe: $!");
-  }
-
-  require IO::Socket::INET;
-  require IO::Socket::SSL;
-
-  my $ex;
-
-  # Fork child
-  $self->handle_sigchld();
-  defined(my $pid = fork()) or die("Can't fork: $!");
-  if ($pid) {
-    eval {
-      # Give the server a chance to start up
-      sleep(2);
-
-      # Manually simulate the STARTTLS protocol
-
-      my $ssl_opts = {
-        SSL_ocsp_mode => IO::Socket::SSL::SSL_OCSP_TRY_STAPLE(),
-        SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE(),
-        SSL_alpn_protocols => [qw(ftp)],
-      };
-
-      starttls_ftp($port, $ssl_opts);
-
-      my $delay = 5;
-      if ($delay > 0) {
-        if ($ENV{TEST_VERBOSE}) {
-          print STDOUT "# Sleeping for $delay seconds\n";
-        }
-
-        sleep($delay);
-      }
-
-      # Do it again, see if we actually read our our cached OCSP response
-      starttls_ftp($port, $ssl_opts);
-    };
-
-    if ($@) {
-      $ex = $@;
-    }
-
-    $wfh->print("done\n");
-    $wfh->flush();
-
-  } else {
-    eval { server_wait($setup->{config_file}, $rfh) };
-    if ($@) {
-      warn($@);
-      exit 1;
-    }
-
-    exit 0;
-  }
-
-  # Stop server
-  server_stop($setup->{pid_file});
-
-  $self->assert_child_ok($pid);
-
-  test_cleanup($setup->{log_file}, $ex);
 }
 
 1;
