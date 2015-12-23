ab90d49dacad604492aa7a029929b38793225988
TJ Saunders <tj@castaglia.org>
2015-12-22 17:04:24 -0800

    Implement an OCSP response cache provider using mod_tls_memcache, with accompanying regression tests.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_tls_memcache.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_tls_memcache.pm
@@ -5,9 +5,11 @@ use base qw(ProFTPD::TestSuite::Child);
 use strict;
 
 use Cache::Memcached;
+use Carp;
 use File::Spec;
 use IO::Handle;
 use IPC::Open3;
+use Socket;
 
 use ProFTPD::TestSuite::FTP;
 use ProFTPD::TestSuite::Utils qw(:auth :config :running :test :testsuite);
@@ -23,6 +25,11 @@ my $TESTS = {
   },
 
   tls_sess_cache_memcache_json_bug4057 => {
+    order => ++$order,
+    test_class => [qw(bug forking)],
+  },
+
+  tls_stapling_on_memcache_bug4175 => {
     order => ++$order,
     test_class => [qw(bug forking)],
   },
@@ -541,6 +548,184 @@ my $openssl = '/Users/tj/local/openssl-1.0.2d/bin/openssl';
 
   } else {
     eval { server_wait($setup->{config_file}, $rfh, 45) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+
+  $self->assert_child_ok($pid);
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub starttls_ftp {
+  my $port = shift;
+  my $ssl_opts = shift;
+
+  my $client = IO::Socket::INET->new(
+    PeerHost => '127.0.0.1',
+    PeerPort => $port,
+    Proto => 'tcp',
+    Type => SOCK_STREAM,
+    Timeout => 10
+  );
+  unless ($client) {
+    croak("Can't connect to 127.0.0.1:$port: $!");
+  }
+
+  # Read the banner
+  my $banner = <$client>;
+  if ($ENV{TEST_VERBOSE}) {
+    print STDOUT "# Received banner: $banner\n";
+  }
+
+  # Send the AUTH command
+  my $cmd = "AUTH TLS\r\n";
+  if ($ENV{TEST_VERBOSE}) {
+    print STDOUT "# Sending command: $cmd";
+  }
+
+  $client->print($cmd);
+  $client->flush();
+
+  # Read the AUTH response
+  my $resp = <$client>;
+  if ($ENV{TEST_VERBOSE}) {
+    print STDOUT "# Received response: $resp\n";
+  }
+
+  my $expected = "234 AUTH TLS successful\r\n";
+  unless ($expected eq $resp) {
+    croak("Expected response '$expected', got '$resp'");
+  }
+
+  # Now perform the SSL handshake
+  if ($ENV{TEST_VERBOSE}) {
+    $IO::Socket::SSL::DEBUG = 3;
+  }
+
+  my $res = IO::Socket::SSL->start_SSL($client, $ssl_opts);
+  unless ($res) {
+    croak("Failed SSL handshake: " . IO::Socket::SSL::errstr());
+  }
+
+  $cmd = "QUIT\r\n";
+  if ($ENV{TEST_VERBOSE}) {
+    print STDOUT "# Sending command: $cmd";
+  }
+
+  print $client $cmd;
+  $client->flush();
+  $client->close();
+}
+
+sub tls_stapling_on_memcache_bug4175 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'tls_memcache');
+
+  my $memcached_servers = $ENV{MEMCACHED_SERVERS} ? $ENV{MEMCACHED_SERVERS} : '127.0.0.1:11211';
+
+  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
+  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+    TraceLog => $setup->{log_file},
+    Trace => 'tls:20 tls.memcache:20',
+
+    AuthUserFile => $setup->{auth_user_file},
+    AuthGroupFile => $setup->{auth_group_file},
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_memcache.c' => {
+        MemcacheEngine => 'on',
+        MemcacheLog => $setup->{log_file},
+        MemcacheServers => $memcached_servers,
+      },
+
+      'mod_tls.c' => {
+        TLSEngine => 'on',
+        TLSLog => $setup->{log_file},
+        TLSProtocol => 'SSLv3 TLSv1',
+        TLSRequired => 'on',
+        TLSRSACertificateFile => $cert_file,
+        TLSCACertificateFile => $ca_file,
+        TLSOptions => 'EnableDiags',
+        TLSStapling => 'on',
+        TLSStaplingCache => "memcache:/",
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require IO::Socket::INET;
+  require IO::Socket::SSL;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      # Give the server a chance to start up
+      sleep(2);
+
+      # Manually simulate the STARTTLS protocol
+
+      my $ssl_opts = {
+        SSL_ocsp_mode => IO::Socket::SSL::SSL_OCSP_TRY_STAPLE(),
+        SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE(),
+        SSL_alpn_protocols => [qw(ftp)],
+      };
+
+      starttls_ftp($port, $ssl_opts);
+
+      my $delay = 5;
+      if ($delay > 0) {
+        if ($ENV{TEST_VERBOSE}) {
+          print STDOUT "# Sleeping for $delay seconds\n";
+        }
+
+        sleep($delay);
+      }
+
+      # Do it again, see if we actually read our our cached OCSP response
+      starttls_ftp($port, $ssl_opts);
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh) };
     if ($@) {
       warn($@);
       exit 1;
