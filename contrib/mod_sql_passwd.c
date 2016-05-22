ae8fc11a1dbecffae751e71a3dcbb9e7fcf8da68
TJ Saunders <tj@castaglia.org>
2016-05-21 19:23:31 -0700

    Address -Wsign-compare compiler warnings in a few more contrib modules.  More still to come.

--- contrib/mod_sql_passwd.c
+++ contrib/mod_sql_passwd.c
@@ -103,10 +103,11 @@ static const char *trace_channel = "sql.passwd";
 /* Necessary prototypes */
 static int sql_passwd_sess_init(void);
 
-static cmd_rec *sql_passwd_cmd_create(pool *parent_pool, int argc, ...) {
+static cmd_rec *sql_passwd_cmd_create(pool *parent_pool,
+    unsigned int argc, ...) {
+  register unsigned int i = 0;
   pool *cmd_pool = NULL;
   cmd_rec *cmd = NULL;
-  register unsigned int i = 0;
   va_list argp;
  
   cmd_pool = make_sub_pool(parent_pool);
