c2ed96feafce29a840232fd055b5edb2c0d2725f
TJ Saunders <tj@castaglia.org>
2016-07-15 13:21:17 -0700

    When checking whether a given filename is a dotfile, make sure to use the actual filename, and not just ASSUME that the client sent the filename (as opposed to a file path).

--- modules/mod_ls.c
+++ modules/mod_ls.c
@@ -450,6 +450,7 @@ static char months[12][4] =
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
 static int listfile(cmd_rec *cmd, pool *p, const char *name) {
+  register unsigned int i;
   int rval = 0, len;
   time_t sort_time;
   char m[PR_TUNABLE_PATH_MAX+1] = {'\0'}, l[PR_TUNABLE_PATH_MAX+1] = {'\0'}, s[16] = {'\0'};
@@ -457,6 +458,29 @@ static int listfile(cmd_rec *cmd, pool *p, const char *name) {
   struct tm *t = NULL;
   char suffix[2];
   int hidden = 0;
+  char *filename, *ptr;
+  size_t namelen;
+
+  /* Note that listfile() expects to be given the file name, NOT the path.
+   * So strip off any path elements, watching out for any trailing slashes
+   * (Bug#4259).
+   */
+  namelen = strlen(name);
+  for (i = namelen-1; i > 0; i--) {
+    if (name[i] != '/') {
+      break;
+    }
+
+    namelen--;
+  }
+
+  filename = pstrndup(p, name, namelen);
+
+  ptr = strrchr(filename, '/');
+  if (ptr != NULL) {
+    /* Advance past that path separator to get just the filename. */
+    filename = ptr + 1;
+  }
 
   if (list_nfiles.curr && list_nfiles.max &&
       list_nfiles.curr >= list_nfiles.max) {
@@ -483,7 +507,7 @@ static int listfile(cmd_rec *cmd, pool *p, const char *name) {
 
 #ifndef PR_USE_NLS
     if (opt_B) {
-      register unsigned int i, j;
+      register unsigned int j;
       size_t display_namelen, printable_namelen;
       char *printable_name = NULL;
 
@@ -597,9 +621,13 @@ static int listfile(cmd_rec *cmd, pool *p, const char *name) {
     }
 
     /* Skip dotfiles, unless requested not to via -a or -A. */
-    if (*name == '.' &&
-        (!opt_a && (!opt_A || is_dotdir(name))))
+    if (*filename == '.' &&
+        (!opt_a && (!opt_A || is_dotdir(filename)))) {
+      pr_log_debug(DEBUG10,
+        "skipping listing of hidden file '%s' (no -A/-a options in effect)",
+        filename);
       return 0;
+    }
 
     if (hidden)
       return 0;
@@ -1891,7 +1919,6 @@ static int dolist(cmd_rec *cmd, const char *opt, int clearflags) {
         }
 
       } else {
-
         /* Trick the following code into using the non-glob() processed path */
         a = 0;
         g.gl_pathv = (char **) pcalloc(cmd->tmp_pool, 2 * sizeof(char *));
@@ -1905,8 +1932,9 @@ static int dolist(cmd_rec *cmd, const char *opt, int clearflags) {
 
       path = g.gl_pathv;
 
-      if (path && path[0] && path[1])
+      if (path && path[0] && path[1]) {
         justone = 0;
+      }
 
       while (path &&
              *path) {
@@ -1927,7 +1955,7 @@ static int dolist(cmd_rec *cmd, const char *opt, int clearflags) {
           }
 
           /* If the -d option is used or the file is not a directory, OR
-           * if the -R option is NOT used AND the file IS a directory AND,
+           * if the -R option is NOT used AND the file IS a directory AND
            * the file is NOT the target/given parameter, then list the file
            * as is.
            */
