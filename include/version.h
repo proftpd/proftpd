9f8be01598cd63c8d42c33398f62c69c45bc203c
TJ Saunders <tj@castaglia.org>
2017-01-14 16:31:56 -0800

    Updating files for release of 1.3.5c.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030508
-#define PROFTPD_VERSION_TEXT		"1.3.5b"
+#define PROFTPD_VERSION_NUMBER		0x0001030509
+#define PROFTPD_VERSION_TEXT		"1.3.5c"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
