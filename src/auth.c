ffa40c955abb7ba499c03b22794eae505c2139c7
TJ Saunders <tj@castaglia.org>
2017-01-15 14:10:34 -0800

    Bug#4283: All FTP logins treated as anonymous logins again.

--- src/auth.c
+++ src/auth.c
@@ -1132,9 +1132,9 @@ int pr_auth_getgroups(pool *p, const char *name, array_header **group_ids,
 }
 
 /* This is one messy function.  Yuck.  Yay legacy code. */
-config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
-    char **user_name, char **anon_name) {
-  config_rec *c = NULL, *topc = NULL;
+config_rec *pr_auth_get_anon_config(pool *p, char **login_user,
+    char **real_user, char **anon_name) {
+  config_rec *c = NULL, *alias_config = NULL, *anon_config = NULL;
   char *config_user_name = NULL, *config_anon_name = NULL;
   unsigned char is_alias = FALSE, *auth_alias_only = NULL;
   unsigned long config_flags = (PR_CONFIG_FIND_FL_SKIP_DIR|PR_CONFIG_FIND_FL_SKIP_LIMIT|PR_CONFIG_FIND_FL_SKIP_DYNDIR);
@@ -1167,12 +1167,14 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
     config_flags);
   if (c != NULL) {
     do {
+      const char *alias;
+
       pr_signals_handle();
 
-      if (strncmp(c->argv[0], "*", 2) == 0 ||
-          strcmp(c->argv[0], *login_name) == 0) {
-        is_alias = TRUE;
-        topc = c;
+      alias = c->argv[0];
+      if (strncmp(alias, "*", 2) == 0 ||
+          strcmp(alias, *login_user) == 0) {
+        alias_config = c;
         break;
       }
 
@@ -1181,7 +1183,9 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
   }
 
   /* This is where things get messy, rapidly. */
-  c = topc;
+  if (is_alias == TRUE) {
+    c = alias_config;
+  }
 
   while (c && c->parent &&
     (auth_alias_only = get_param_ptr(c->parent->subset, "AuthAliasOnly", FALSE))) {
@@ -1204,7 +1208,7 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
 
     is_alias = FALSE;
 
-    find_config_set_top(topc);
+    find_config_set_top(alias_config);
     c = find_config_next2(c, c->next, CONF_PARAM, "UserAlias", TRUE,
       config_flags);
 
@@ -1212,6 +1216,7 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
         (strncmp(c->argv[0], "*", 2) == 0 ||
          strcmp(c->argv[0], *login_name) == 0)) {
       is_alias = TRUE;
+      alias_config = c;
     }
   }
 
@@ -1219,11 +1224,11 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
     *login_name = c->argv[1];
 
     /* If the alias is applied inside an <Anonymous> context, we have found
-     * our anon block.
+     * our <Anonymous> section.
      */
     if (c->parent &&
         c->parent->config_type == CONF_ANON) {
-      c = c->parent;
+      anon_config = c->parent;
 
     } else {
       c = NULL;
@@ -1231,15 +1236,18 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
   }
 
   /* Next, search for an anonymous entry. */
-
-  if (c == NULL) {
+  if (anon_config == NULL) {
     c = find_config(main_server->conf, CONF_ANON, NULL, FALSE);
 
   } else {
-    find_config_set_top(c);
+    find_config_set_top(anon_config);
+    c = anon_config;
   }
 
-  if (c) {
+  /* If anon_config is null here but c is not null, then we may have found
+   * a candidate <Anonymous> section.  Let's examine it more closely.
+   */
+  if (c != NULL) {
     config_rec *starting_c;
 
     starting_c = c;
@@ -1252,11 +1260,15 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
       }
 
       if (config_anon_name != NULL &&
-          strcmp(config_anon_name, *login_name) == 0) {
-         if (anon_name != NULL) {
-           *anon_name = config_anon_name;
-         }
-         break;
+          strcmp(config_anon_name, *login_user) == 0) {
+
+        /* We found our <Anonymous> section. */
+        anon_config = c;
+
+        if (anon_name != NULL) {
+          *anon_name = config_anon_name;
+        }
+        break;
       }
  
     } while ((c = find_config_next(c, c->next, CONF_ANON, NULL,
@@ -1265,7 +1277,7 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
     c = starting_c;
   }
 
-  if (!is_alias) {
+  if (is_alias == FALSE) {
     /* Yes, we do want to be using c here.  Otherwise, we risk a regression
      * of Bug#3501.
      */
@@ -1288,20 +1300,42 @@ config_rec *pr_auth_get_anon_config(pool *p, char **login_name,
           FALSE);
       }
 
-      if (*login_name &&
-          auth_alias_only &&
+      if (login_user != NULL &&
+          auth_alias_only != NULL &&
           *auth_alias_only == TRUE) {
         *login_name = NULL;
       }
 
-      if ((!login_name || !c) &&
-          anon_name) {
+      if ((login_user == NULL || anon_config == NULL) &&
+          anon_name != NULL) {
         *anon_name = NULL;
+      }
+    }
+
+  } else {
+    config_rec *alias_parent_config = NULL;
+
+    /* We have found a matching UserAlias for the USER name sent by the client.
+     * But we need to properly handle any AuthAliasOnly directives in that
+     * config as well (Bug#2070).
+     */
+    if (alias_config != NULL) {
+      alias_parent_config = alias_config->parent;
+    }
+
+    auth_alias_only = get_param_ptr(alias_parent_config ?
+      alias_parent_config->subset : main_server->conf, "AuthAliasOnly", FALSE);
+
+    if (auth_alias_only != NULL &&
+        *auth_alias_only == TRUE) {
+      if (alias_parent_config != NULL &&
+          alias_parent_config->config_type == CONF_ANON) {
+        anon_config = alias_parent_config;
       }
     }
   }
 
-  return c;
+  return anon_config;
 }
 
 int pr_auth_banned_by_ftpusers(xaset_t *ctx, const char *user) {
