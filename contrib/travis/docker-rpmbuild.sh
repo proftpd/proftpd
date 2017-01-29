38da8defad466fb31c1e20dd722abe14df97020c
TJ Saunders <tj@castaglia.org>
2017-01-28 23:42:53 -0800

    Now when building the RPM, use `--with everything`.

--- contrib/travis/docker-rpmbuild.sh
+++ contrib/travis/docker-rpmbuild.sh
@@ -7,7 +7,12 @@ exit 0
 
 # Make sure that the necessary packages/tools are installed
 yum install -y gcc make git rpm-build
+
+# These are for the basic proftpd build
 yum install -y gettext pkgconfig pam-devel ncurses-devel zlib-devel libacl-devel libcap-devel
+
+# And these are for --with everything
+yum install -y openldap-devel libmemcached-devel mysql-devel pcre-devel postgresql-devel openssl-devel tcp_wrappers-devel
 
 mkdir git
 cd git
@@ -18,4 +23,4 @@ rm -fr .git/
 make dist
 cd ..
 tar zcvf proftpd-${VERSION}.tar.gz proftpd-${VERSION}
-rpmbuild -ta proftpd-${VERSION}.tar.gz
+rpmbuild -ta proftpd-${VERSION}.tar.gz --with everything
