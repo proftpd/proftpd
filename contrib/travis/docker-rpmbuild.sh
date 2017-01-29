86009875fb19ed3330d0879c53f4d4debd9f2f0e
TJ Saunders <tj@castaglia.org>
2017-01-28 20:41:46 -0800

    Attempting to add support for building an RPM, using the provided proftpd.spec file, via Travis.  This requires us to use Docker to get a Centos container for doing the rpmbuild.

---
+++ contrib/travis/docker-rpmbuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+VERSION=1.3.6rc5
+
+# Make sure that the necessary packages/tools are installed
+yum install -y git rpm-build
+yum install -y gcc make gettext pam-devel ncurses-devel zlib-devel libacl-devel libcap-devel
+
+mkdir git
+cd git
+git clone --depth 10 https://github.com/proftpd/proftpd.git proftpd-${VERSION}
+cd proftpd-${VERSION}/
+./configure
+rm -fr .git/
+make dist
+cd ..
+tar zcvf proftpd-${VERSION}.tar.gz proftpd-${VERSION}
+rpmbuild -ta proftpd-${VERSION}.tar.gz
