c57792c857114a2da733e3102b6884aad3e61d88
TJ Saunders <tj@castaglia.org>
2017-01-29 21:02:13 -0800

    Updated the RPM .spec file to include/build more of the newer ProFTPD modules. Update the repo layout for distribution-related tools, to be more consistent.

--- contrib/dist/travis/docker-rpmbuild.sh
+++ contrib/dist/travis/docker-rpmbuild.sh
@@ -9,7 +9,13 @@ yum install -y gcc make git rpm-build
 yum install -y gettext pkgconfig pam-devel ncurses-devel zlib-devel libacl-devel libcap-devel
 
 # And these are for --with everything
-yum install -y openldap-devel libmemcached-devel mysql-devel pcre-devel postgresql-devel openssl-devel tcp_wrappers-devel
+yum install -y openldap-devel libmemcached-devel mysql-devel pcre-devel postgresql-devel openssl-devel tcp_wrappers-devel sqlite-devel geoip-devel
+
+# Install the EPEL repo, for the Redis RPMs
+yum install -y wget
+wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/e
+rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-*.rpm
+yum install -y hiredis-devel
 
 mkdir git
 cd git
