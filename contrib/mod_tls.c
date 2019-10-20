edc11b4b013aedcee45bf6754027c97e5ee45b8c
TJ Saunders <tj@castaglia.org>
2019-10-19 17:00:06 -0700

    Backport of fix for Issue #810 to the 1.3.6 branch.

--- contrib/mod_tls.c
+++ contrib/mod_tls.c
@@ -1565,7 +1565,8 @@ static void tls_msg_cb(int io_flag, int version, int content_type,
       version_str = "TLSv1.2";
       break;
 
-# if OPENSSL_VERSION_NUMBER >= 0x10101000L
+# if OPENSSL_VERSION_NUMBER >= 0x10101000L && \
+     defined(TLS1_3_VERSION)
     case TLS1_3_VERSION:
       version_str = "TLSv1.3";
       break;
@@ -1592,7 +1593,8 @@ static void tls_msg_cb(int io_flag, int version, int content_type,
 #if OPENSSL_VERSION_NUMBER >= 0x10001000L
       version == TLS1_1_VERSION ||
       version == TLS1_2_VERSION ||
-# if OPENSSL_VERSION_NUMBER >= 0x10101000L
+# if OPENSSL_VERSION_NUMBER >= 0x10101000L && \
+     defined(TLS1_3_VERSION)
       version == TLS1_3_VERSION ||
 # endif
 #endif
@@ -7932,8 +7934,7 @@ static int tls_dotlogin_allow(const char *user) {
     file_sig = file_cert->signature;
 #endif /* OpenSSL-1.1.x and later */
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L && \
-    !defined(HAVE_LIBRESSL)
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
     if (!ASN1_STRING_cmp(client_sig, file_sig)) {
 #else
     if (!M_ASN1_BIT_STRING_cmp(client_sig, file_sig)) {
