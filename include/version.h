d61efd40d18adb34ef615fe4bf4345278273c279
TJ Saunders <tj@castaglia.org>
2020-04-16 07:57:41 -0700

    Adding missing guards on two headers, to prevent multiple inclusion.

--- include/version.h
+++ include/version.h
@@ -1,3 +1,30 @@
+/*
+ * ProFTPD - FTP server daemon
+ * Copyright (c) 2020 The ProFTPD Project team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+ *
+ * As a special exemption, Public Flood Software/MacGyver aka Habeeb J. Dihu
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
+#ifndef PR_VERSION_H
+#define PR_VERSION_H
+
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
@@ -13,3 +40,5 @@ const char *pr_version_get_str(void);
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
 #define PR_STATUS          		"(git)"
+
+#endif /* PR_VERSION_H */
