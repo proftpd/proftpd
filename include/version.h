7380b909207ce9bbf3df047a30e682513fba4394
TJ Saunders <tj@castaglia.org>
2020-07-20 22:05:07 -0700

    Preparing for start of 1.3.8 development cycle.

--- include/version.h
+++ include/version.h
@@ -28,8 +28,8 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030705
-#define PROFTPD_VERSION_TEXT		"1.3.7"
+#define PROFTPD_VERSION_NUMBER		0x0001030801
+#define PROFTPD_VERSION_TEXT		"1.3.8rc1"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
@@ -39,6 +39,6 @@ unsigned long pr_version_get_number(void);
 const char *pr_version_get_str(void);
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(stable)"
+#define PR_STATUS          		"(git)"
 
 #endif /* PR_VERSION_H */
