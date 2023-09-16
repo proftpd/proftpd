6d37dfd814480b635d0ecf1b2fe57e2a7996cfdb
TJ Saunders <tj@castaglia.org>
2023-09-16 09:30:13 -0700

    Issue #1712: Handle the `SFTPCiphers`, `SFTPDigests`, `SFTPKeyExchang… (#1718)
    * Issue #1712: Handle the `SFTPCiphers`, `SFTPDigests`, `SFTPKeyExchanges`, and `SFTPHostKeys` directively normally, reverting some of the changes from Issue #1444.
    
    The use of the added `create_config()` function, for "embedding" a config_rec
    in the `SFTPClientMatch` config_rec for Issue #1444 worked, but was *not* needed
    in the directive handlers for the directives, as for
    non-`SFTPClientMatch`-using configurations.
    
    * Add a regression test for this, to catch/prevent future regressions.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_sftp.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_sftp.pm
@@ -1936,6 +1936,11 @@ my $TESTS = {
     test_class => [qw(bug forking sftp ssh2)],
   },
 
+  sftp_config_global_algos_issue1712 => {
+    order => ++$order,
+    test_class => [qw(bug forking sftp ssh2)],
+  },
+
 };
 
 sub get_sftplog {
@@ -66983,6 +66988,111 @@ sub sftp_ext_hostkey_rotation_issue1323 {
   if ($@) {
     $ex = $@;
   }
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub sftp_config_global_algos_issue1712 {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'sftp');
+
+  my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_rsa_key');
+  my $dsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_sftp/ssh_host_dsa_key');
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+    TraceLog => $setup->{log_file},
+    Trace => 'ssh2:20 sftp:20',
+
+    AuthUserFile => $setup->{auth_user_file},
+    AuthGroupFile => $setup->{auth_group_file},
+    AuthOrder => 'mod_auth_file.c',
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_sftp.c' => [
+        "SFTPEngine on",
+        "SFTPLog $setup->{log_file}",
+        "SFTPHostKey $rsa_host_key",
+        "SFTPHostKey $dsa_host_key",
+      ],
+    },
+
+    Global => {
+      SFTPCiphers => 'arcfour128',
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::SSH2;
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $ssh2_opts = {};
+
+      if ($ENV{TEST_VERBOSE}) {
+        $ssh2_opts->{trace} = -1;
+      }
+
+      my $ssh2 = Net::SSH2->new(%$ssh2_opts);
+
+      sleep(1);
+
+      my $cipher = 'arcfour128';
+      $ssh2->method('crypt_cs', $cipher);
+
+      unless ($ssh2->connect('127.0.0.1', $port)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't connect to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      my $cipher_used = $ssh2->method('crypt_cs');
+      $self->assert($cipher eq $cipher_used,
+        test_msg("Expected cipher '$cipher', got '$cipher_used'"));
+
+      $ssh2->disconnect();
+    };
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+  $self->assert_child_ok($pid);
 
   test_cleanup($setup->{log_file}, $ex);
 }
