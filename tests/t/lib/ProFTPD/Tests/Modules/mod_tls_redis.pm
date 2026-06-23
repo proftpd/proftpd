f3a0f7bb6009e4cd9014fffbff5b4c9860c5d9ce
TJ Saunders <tj@castaglia.org>
2026-06-23 14:37:34 -0700

    Issue #2166: When decoding the base64-encoded JSON data from backend Memcached/Redis servers for cached TLS sessions or OCSP responses, perform bounds checking.

--- tests/t/lib/ProFTPD/Tests/Modules/mod_tls_redis.pm
+++ tests/t/lib/ProFTPD/Tests/Modules/mod_tls_redis.pm
@@ -1,14 +1,14 @@
-package ProFTPD::Tests::Modules::mod_tls_memcache;
+package ProFTPD::Tests::Modules::mod_tls_redis;
 
 use lib qw(t/lib);
 use base qw(ProFTPD::TestSuite::Child);
 use strict;
 
-use Cache::Memcached;
 use Carp;
 use File::Spec;
 use IO::Handle;
 use IPC::Open3;
+use Redis;
 use Socket;
 
 use ProFTPD::TestSuite::FTP;
@@ -19,19 +19,14 @@ $| = 1;
 my $order = 0;
 
 my $TESTS = {
-  tls_sess_cache_memcache => {
+  tls_sess_cache_redis => {
     order => ++$order,
     test_class => [qw(forking)],
   },
 
-  tls_sess_cache_memcache_json_bug4057 => {
+  tls_stapling_on_redis => {
     order => ++$order,
-    test_class => [qw(bug forking)],
-  },
-
-  tls_stapling_on_memcache_bug4175 => {
-    order => ++$order,
-    test_class => [qw(bug forking)],
+    test_class => [qw(forking)],
   },
 
 };
@@ -44,27 +39,25 @@ sub set_up {
   my $self = shift;
   $self->SUPER::set_up(@_);
 
-  # Clear the memcached servers before each unit test
-  my $memcached_servers = $ENV{MEMCACHED_SERVERS} ? $ENV{MEMCACHED_SERVERS} : "127.0.0.1:11211";
-  unless ($memcached_servers =~ /:/) {
-    $memcached_servers .= ':11211';
+  # Clear the Redis server before each unit test
+  my $redis_server = $ENV{REDIS_SERVER} ? $ENV{REDIS_SERVER} : "127.0.0.1:6379";
+  unless ($redis_server =~ /:/) {
+    $redis_server .= ':6379';
   }
 
-  $memcached_servers = [split(/,?\s+?/, $memcached_servers)];
-
-  my $mc = Cache::Memcached->new({
-    servers => $memcached_servers,
+  my $redis = Redis->new(
+    server => $redis_server,
     debug => 0,
-  });
+  );
 
-  # First, make sure that a memcached is running
-  my $stats = $mc->stats('misc');
-  unless ($stats) {
-    die("Can't obtain stats from memached servers '$memcached_servers'");
+  # First, make sure that a Redis is running
+  my $pong = $redis->ping();
+  unless ($pong) {
+    die("Can't ping Redis server '$redis_server'");
   }
 
-  $mc->flush_all();
-  $mc->disconnect_all();
+  $redis->flushall();
+  $redis->quit();
 }
 
 sub list_tests {
@@ -96,12 +89,12 @@ sub list_tests {
   return testsuite_get_runnable_tests($TESTS);
 }
 
-sub tls_sess_cache_memcache {
+sub tls_sess_cache_redis {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
-  my $setup = test_setup($tmpdir, 'tls_memcache');
+  my $setup = test_setup($tmpdir, 'tls_redis');
 
-  my $memcached_servers = $ENV{MEMCACHED_SERVERS} ? $ENV{MEMCACHED_SERVERS} : '127.0.0.1:11211';
+  my $redis_server = $ENV{REDIS_SERVER} ? $ENV{REDIS_SERVER} : '127.0.0.1:6379';
 
   my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
   my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
@@ -113,7 +106,7 @@ sub tls_sess_cache_memcache {
     ScoreboardFile => $setup->{scoreboard_file},
     SystemLog => $setup->{log_file},
     TraceLog => $setup->{log_file},
-    Trace => 'tls:20 memcache:30 tls.memcache:20',
+    Trace => 'tls:20 redis:30 tls.redis:20',
 
     AuthUserFile => $setup->{auth_user_file},
     AuthGroupFile => $setup->{auth_group_file},
@@ -124,10 +117,10 @@ sub tls_sess_cache_memcache {
         DelayEngine => 'off',
       },
 
-      'mod_memcache.c' => {
-        MemcacheEngine => 'on',
-        MemcacheLog => $setup->{log_file},
-        MemcacheServers => $memcached_servers,
+      'mod_redis.c' => {
+        RedisEngine => 'on',
+        RedisLog => $setup->{log_file},
+        RedisServer => $redis_server,
       },
 
       'mod_tls.c' => {
@@ -143,252 +136,8 @@ sub tls_sess_cache_memcache {
         TLSProtocol => 'TLSv1.2',
       },
 
-      'mod_tls_memcache.c' => {
-        TLSSessionCache => 'memcache:',
-      },
-    },
-  };
-
-  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
-    $config);
-
-  # Open pipes, for use between the parent and child processes.  Specifically,
-  # the child will indicate when it's done with its test by writing a message
-  # to the parent.
-  my ($rfh, $wfh);
-  unless (pipe($rfh, $wfh)) {
-    die("Can't open pipe: $!");
-  }
-
-  my $ex;
-
-  # Fork child
-  $self->handle_sigchld();
-  defined(my $pid = fork()) or die("Can't fork: $!");
-  if ($pid) {
-    eval {
-      # Give the server a chance to start up
-      sleep(2);
-
-      # To test SSL session resumption, we use the command-line
-      # openssl s_client tool, rather than any Perl module.
-
-      # XXX Some OpenSSL versions' of s_client do not support the 'ftp'
-      # parameter for -starttls; in this case, point the openssl binary
-      # to be used to a version which does support this.
-      my $openssl = 'openssl';
-
-      my @cmd = (
-        $openssl,
-        's_client',
-        '-connect',
-        "127.0.0.1:$port",
-        '-starttls',
-        'ftp',
-        '-sess_out',
-        $sessid_file,
-        '-CAfile',
-        $ca_file,
-        '-noservername',
-        '-no_check_time',
-      );
-
-      my $tls_rh = IO::Handle->new();
-      my $tls_wh = IO::Handle->new();
-      my $tls_eh = IO::Handle->new();
-
-      $tls_wh->autoflush(1);
-
-      local $SIG{CHLD} = 'DEFAULT';
-
-      if ($ENV{TEST_VERBOSE}) {
-        print STDERR "Executing: ", join(' ', @cmd), "\n";
-      }
-
-      my $tls_pid = open3($tls_wh, $tls_rh, $tls_eh, @cmd);
-      print $tls_wh "QUIT\r\n";
-      waitpid($tls_pid, 0);
-
-      my ($res, $cipher_str, $err_str, $out_str);
-      if ($? >> 8) {
-        $err_str = join('', <$tls_eh>);
-        $res = 0;
-
-      } else {
-        my $output = [<$tls_rh>];
-
-        # Specifically look for the line containing 'Cipher is'
-        foreach my $line (@$output) {
-          if ($line =~ /Cipher is/) {
-            $cipher_str = $line;
-            chomp($cipher_str);
-          }
-        }
-
-        if ($ENV{TEST_VERBOSE}) {
-          $out_str = join('', @$output);
-          print STDERR "Stdout: $out_str\n";
-        }
-
-        if ($ENV{TEST_VERBOSE}) {
-          $err_str = join('', <$tls_eh>);
-          print STDERR "Stderr: $err_str\n";
-        }
-
-        $res = 1;
-      }
-
-      unless ($res) {
-        die("Can't talk to server: $err_str");
-      }
-
-      my $expected = '^New';
-      $self->assert(qr/$expected/, $cipher_str,
-        test_msg("Expected '$expected', got '$cipher_str'"));
-
-      @cmd = (
-        $openssl,
-        's_client',
-        '-connect',
-        "127.0.0.1:$port",
-        '-starttls',
-        'ftp',
-        '-sess_in',
-        $sessid_file,
-        '-CAfile',
-        $ca_file,
-        '-noservername',
-        '-no_check_time',
-      );
-
-      $tls_rh = IO::Handle->new();
-      $tls_wh = IO::Handle->new();
-      $tls_eh = IO::Handle->new();
-
-      $tls_wh->autoflush(1);
-
-      if ($ENV{TEST_VERBOSE}) {
-        print STDERR "Executing: ", join(' ', @cmd), "\n";
-      }
-
-      $tls_pid = open3($tls_wh, $tls_rh, $tls_eh, @cmd);
-      print $tls_wh "QUIT\r\n";
-      waitpid($tls_pid, 0);
-
-      $res = 0;
-      $cipher_str = undef;
-      $err_str = undef;
-      $out_str = undef;
-
-      if ($? >> 8) {
-        $err_str = join('', <$tls_eh>);
-        $res = 0;
-
-      } else {
-        my $output = [<$tls_rh>];
-
-        # Specifically look for the line containing 'Cipher is'
-        foreach my $line (@$output) {
-          if ($line =~ /Cipher is/) {
-            $cipher_str = $line;
-            chomp($cipher_str);
-          }
-        }
-
-        if ($ENV{TEST_VERBOSE}) {
-          $out_str = join('', @$output);
-          print STDERR "Stdout: $out_str\n";
-        }
-
-        if ($ENV{TEST_VERBOSE}) {
-          $err_str = join('', <$tls_eh>);
-          print STDERR "Stderr: $err_str\n";
-        }
-
-        $res = 1;
-      }
-
-      unless ($res) {
-        die("Can't talk to server: $err_str");
-      }
-
-      $expected = '^Reused';
-      $self->assert(qr/$expected/, $cipher_str,
-        test_msg("Expected '$expected', got '$cipher_str'"));
-    };
-    if ($@) {
-      $ex = $@;
-    }
-
-    $wfh->print("done\n");
-    $wfh->flush();
-
-  } else {
-    eval { server_wait($setup->{config_file}, $rfh, 45) };
-    if ($@) {
-      warn($@);
-      exit 1;
-    }
-
-    exit 0;
-  }
-
-  # Stop server
-  server_stop($setup->{pid_file});
-  $self->assert_child_ok($pid);
-
-  test_cleanup($setup->{log_file}, $ex);
-}
-
-sub tls_sess_cache_memcache_json_bug4057 {
-  my $self = shift;
-  my $tmpdir = $self->{tmpdir};
-  my $setup = test_setup($tmpdir, 'tls_memcache');
-
-  my $memcached_servers = $ENV{MEMCACHED_SERVERS} ? $ENV{MEMCACHED_SERVERS} : '127.0.0.1:11211';
-
-  my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
-  my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
-
-  my $sessid_file = File::Spec->rel2abs("$tmpdir/sessid.pem");
-
-  my $config = {
-    PidFile => $setup->{pid_file},
-    ScoreboardFile => $setup->{scoreboard_file},
-    SystemLog => $setup->{log_file},
-    TraceLog => $setup->{log_file},
-    Trace => 'tls:20 memcache:30 tls.memcache:20',
-
-    AuthUserFile => $setup->{auth_user_file},
-    AuthGroupFile => $setup->{auth_group_file},
-    AuthOrder => 'mod_auth_file.c',
-
-    IfModules => {
-      'mod_delay.c' => {
-        DelayEngine => 'off',
-      },
-
-      'mod_memcache.c' => {
-        MemcacheEngine => 'on',
-        MemcacheLog => $setup->{log_file},
-        MemcacheServers => $memcached_servers,
-      },
-
-      'mod_tls.c' => {
-        TLSEngine => 'on',
-        TLSLog => $setup->{log_file},
-        TLSRequired => 'on',
-        TLSRSACertificateFile => $cert_file,
-        TLSCACertificateFile => $ca_file,
-        TLSVerifyClient => 'off',
-        TLSOptions => 'EnableDiags',
-
-        # Necessary for session ID caching; TLSv1.3 uses session tickets
-        TLSProtocol => 'TLSv1.2',
-      },
-
-      'mod_tls_memcache.c' => {
-        TLSSessionCache => 'memcache:/json',
+      'mod_tls_redis.c' => {
+        TLSSessionCache => 'redis:',
       },
     },
   };
@@ -645,12 +394,12 @@ sub starttls_ftp {
   $client->close();
 }
 
-sub tls_stapling_on_memcache_bug4175 {
+sub tls_stapling_on_redis {
   my $self = shift;
   my $tmpdir = $self->{tmpdir};
-  my $setup = test_setup($tmpdir, 'tls_memcache');
+  my $setup = test_setup($tmpdir, 'tls_redis');
 
-  my $memcached_servers = $ENV{MEMCACHED_SERVERS} ? $ENV{MEMCACHED_SERVERS} : '127.0.0.1:11211';
+  my $redis_server = $ENV{REDIS_SERVER} ? $ENV{REDIS_SERVER} : '127.0.0.1:6379';
 
   my $cert_file = File::Spec->rel2abs('t/etc/modules/mod_tls/server-cert.pem');
   my $ca_file = File::Spec->rel2abs('t/etc/modules/mod_tls/ca-cert.pem');
@@ -660,7 +409,7 @@ sub tls_stapling_on_memcache_bug4175 {
     ScoreboardFile => $setup->{scoreboard_file},
     SystemLog => $setup->{log_file},
     TraceLog => $setup->{log_file},
-    Trace => 'tls:20 tls.memcache:20',
+    Trace => 'tls:20 redis:20 tls.redis:20',
 
     AuthUserFile => $setup->{auth_user_file},
     AuthGroupFile => $setup->{auth_group_file},
@@ -671,10 +420,10 @@ sub tls_stapling_on_memcache_bug4175 {
         DelayEngine => 'off',
       },
 
-      'mod_memcache.c' => {
-        MemcacheEngine => 'on',
-        MemcacheLog => $setup->{log_file},
-        MemcacheServers => $memcached_servers,
+      'mod_redis.c' => {
+        RedisEngine => 'on',
+        RedisLog => $setup->{log_file},
+        RedisServer => $redis_server,
       },
 
       'mod_tls.c' => {
@@ -685,7 +434,10 @@ sub tls_stapling_on_memcache_bug4175 {
         TLSCACertificateFile => $ca_file,
         TLSOptions => 'EnableDiags',
         TLSStapling => 'on',
-        TLSStaplingCache => "memcache:/",
+      },
+
+      'mod_tls_redis.c' => {
+        TLSStaplingCache => 'redis:',
       },
     },
   };
