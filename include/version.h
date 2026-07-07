0ee161a750c04f9b913eb53a4f0c745b1c40f270
TJ Saunders <tj@castaglia.org>
2026-07-07 13:13:24 -0700

    Rev the version text in the repo, for whenever the next RC is made.

--- include/version.h
+++ include/version.h
@@ -27,8 +27,8 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030A03
-#define PROFTPD_VERSION_TEXT		"1.3.10rc3"
+#define PROFTPD_VERSION_NUMBER		0x0001030A04
+#define PROFTPD_VERSION_TEXT		"1.3.10rc4"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
@@ -38,6 +38,6 @@ unsigned long pr_version_get_number(void);
 const char *pr_version_get_str(void);
 
 /* PR_STATUS is reported by --version-status -- don't ask why */
-#define PR_STATUS          		"(devel)"
+#define PR_STATUS          		"(git)"
 
 #endif /* PR_VERSION_H */
