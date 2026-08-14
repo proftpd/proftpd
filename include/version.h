f59f248e26bdfa514443e42bf72a145641df635d
TJ Saunders <tj@castaglia.org>
2026-08-14 11:18:44 -0700

    Preparing for release of 1.3.9d.

--- include/version.h
+++ include/version.h
@@ -28,8 +28,8 @@
 #include "buildstamp.h"
 
 /* Application version (in various forms) */
-#define PROFTPD_VERSION_NUMBER		0x0001030907
-#define PROFTPD_VERSION_TEXT		"1.3.9c"
+#define PROFTPD_VERSION_NUMBER		0x0001030908
+#define PROFTPD_VERSION_TEXT		"1.3.9d"
 
 /* Module API version */
 #define PR_MODULE_API_VERSION		0x20
