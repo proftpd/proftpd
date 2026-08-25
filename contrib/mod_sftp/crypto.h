0b60a20f22e3f0b376639b7938955ccf5670dfa4
TJ Saunders <tj@castaglia.org>
2026-08-25 10:18:51 -0700

    Refactor the construction of the key exchange and hostkey algorithm lists for SSH KEXINIT so that all algorithm lists are in the same place.
    Previously, these lists were spread between `kex.c` and `crypto.c`, leading to some unexpected duplication when adding new algorithms.

--- contrib/mod_sftp/crypto.h
+++ contrib/mod_sftp/crypto.h
@@ -44,6 +44,8 @@ int sftp_crypto_set_driver(const char *driver_name);
 /* Return the comma-separated lists of algorithms for KEXINIT. */
 const char *sftp_crypto_get_kexinit_cipher_list(pool *p);
 const char *sftp_crypto_get_kexinit_digest_list(pool *p);
+const char *sftp_crypto_get_kexinit_key_exchange_list(pool *p);
+const char *sftp_crypto_get_kexinit_hostkey_list(pool *p);
 
 size_t sftp_crypto_get_size(size_t, size_t);
 
